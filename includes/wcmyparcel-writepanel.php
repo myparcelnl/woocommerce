71eebf807684beb76223ba8b2e3562c0c969449c
Ewout Fernhout <chocolade@extrapuur.nl>
2015-11-06 12:58:37 +0100

    add header to allow cross origin access to passdata file

--- includes/wcmyparcel-writepanel.php
+++ includes/wcmyparcel-writepanel.php
@@ -209,7 +209,7 @@ class WC_MyParcel_Writepanel {
 		$username = $this->settings['api_username'];
 		$api_key = $this->settings['api_key'];
 
-		$webshop = trailingslashit( plugin_dir_url( __FILE__ ) ) . 'wcmyparcel-pakjegemak-passdata.html';
+		$webshop = trailingslashit( plugin_dir_url( __FILE__ ) ) . 'wcmyparcel-pakjegemak-passdata.php';
 		$hash = hash_hmac('sha1', $username . 'MyParcel' . $webshop, $api_key);
 
 		// check for secure context
