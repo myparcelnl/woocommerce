6ee476e0e2c448e0707bd38c669e3bd1d37ceb44
Ewout Fernhout <chocolade@extrapuur.nl>
2016-10-25 16:47:42 +0200

    shipping method title fallback

--- includes/class-wcmp-settings-callbacks.php
+++ includes/class-wcmp-settings-callbacks.php
@@ -243,7 +243,8 @@ class WooCommerce_MyParcel_Settings_Callbacks {
 				$shipping_methods_selected = (array) $current;
 				$shipping_methods = WC()->shipping->load_shipping_methods();
 				if ( $shipping_methods ) foreach ( $shipping_methods as $key => $shipping_method ) {
-					echo '<option value="' . esc_attr( $key ) . '"' . selected( in_array( $key, $shipping_methods_selected ), true, false ) . '>' . esc_html( $shipping_methods[$key]->title ) . '</option>';
+					$method_title = !empty($shipping_methods[$key]->title) ? $shipping_methods[$key]->title : $shipping_methods[$key]->method_title;
+					echo '<option value="' . esc_attr( $key ) . '"' . selected( in_array( $key, $shipping_methods_selected ), true, false ) . '>' . esc_html( $method_title ) . '</option>';
 				}
 			?>
 		</select>
