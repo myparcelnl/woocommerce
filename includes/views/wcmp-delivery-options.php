45b70fc576e4e23091716d1313f68ee6fde1268e
Ewout Fernhout <chocolade@extrapuur.nl>
2016-11-18 15:53:21 +0100

    theme conflict fix: don't load scripts via wp_enqueue_script

--- includes/views/wcmp-delivery-options.php
+++ includes/views/wcmp-delivery-options.php
@@ -2,7 +2,7 @@
 /**
  * LOAD WORDPRESS
  */
-// define('WP_USE_THEMES', false);
+define('WP_USE_THEMES', false);
 require( '../../../../../wp-blog-header.php');
 ?>
 <html>
@@ -11,40 +11,18 @@ require( '../../../../../wp-blog-header.php');
 	// Delivery options template
 	include('wcmp-delivery-options-template.php');
 
-	// MyParcel bundled scripts
-	wp_enqueue_script(
-		'wc-myparcel-delivery-options-moment',
-		WooCommerce_MyParcel()->plugin_url() . '/assets/delivery-options/js/moment.min.js',
-		array( 'jquery' ),
-		WC_MYPARCEL_VERSION
-	);
-	wp_enqueue_script(
-		'wc-myparcel-delivery-options-webcomponents',
-		WooCommerce_MyParcel()->plugin_url() . '/assets/delivery-options/js/webcomponents.min.js',
-		array(),
-		WC_MYPARCEL_VERSION
-	);
-	wp_enqueue_script(
-		'wc-myparcel-delivery-options',
-		WooCommerce_MyParcel()->plugin_url() . '/assets/delivery-options/js/myparcel.js',
-		array( 'jquery','wc-myparcel-delivery-options-webcomponents','wc-myparcel-delivery-options-moment' ),
-		WC_MYPARCEL_VERSION
-	);
+	// define script & style formats
+	$script_format = '<script type="text/javascript" src="%s"></script>';
+	$style_format = '<link rel="stylesheet" id="%s" href="%s" type="text/css" media="all">';
 
-	wp_enqueue_script(
-		'wc-myparcel-frontend',
-		WooCommerce_MyParcel()->plugin_url() . '/assets/js/wcmp-frontend-iframe.js',
-		array( 'jquery' ),
-		WC_MYPARCEL_VERSION
-	);
+	// load jquery
+	printf( $script_format, includes_url( 'js/jquery/jquery.js' ) );
 
-	wp_enqueue_script( 'jquery' );
-
-	// wp_enqueue_style( 'colors' );
-	// wp_enqueue_style( 'media' );
-
-	// load WordPress header (enqueued scripts etc.)
-	wp_head();
+	// MyParcel scripts
+	printf( $script_format, add_query_arg( 'ver', WC_MYPARCEL_VERSION, WooCommerce_MyParcel()->plugin_url() . '/assets/delivery-options/js/moment.min.js' ) );
+	printf( $script_format, add_query_arg( 'ver', WC_MYPARCEL_VERSION, WooCommerce_MyParcel()->plugin_url() . '/assets/delivery-options/js/webcomponents.min.js' ) );
+	printf( $script_format, add_query_arg( 'ver', WC_MYPARCEL_VERSION, WooCommerce_MyParcel()->plugin_url() . '/assets/delivery-options/js/myparcel.js' ) );
+	printf( $script_format, add_query_arg( 'ver', WC_MYPARCEL_VERSION, WooCommerce_MyParcel()->plugin_url() . '/assets/js/wcmp-frontend-iframe.js' ) );
 	?>
 	<style>
 		body{
