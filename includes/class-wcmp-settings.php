2253d5858777289e6b6af6a1410dc0d8bb467d90
Richard Perdaan <richardperdaan@live.nl>
2018-01-31 14:11:04 +0100

    fix transluation hs-code

--- includes/class-wcmp-settings.php
+++ includes/class-wcmp-settings.php
@@ -548,7 +548,7 @@ class WooCommerce_MyParcel_Settings {
 				'option_name'	=> $option_name,
 				'id'			=> 'hs_code',
 				'size'			=> '5',
-				'description'	=> __( sprintf( 'You can find HS codes on the %ssite of the Dutch Customs%s.', '<a href="http://tarief.douane.nl/tariff/index.jsf" target="_blank">', '</a>' ), 'woocommerce-myparcel' ),
+				'description'	=> sprintf(__( 'HS Codes are used for MyParcel world shipments, you can find the appropriate code on the %ssite of the Dutch Customs%s.', 'woocommerce-myparcel' ), '<a href="http://tarief.douane.nl/tariff/index.jsf" target="_blank">', '</a>')
 			)
 		);
 		add_settings_field(
