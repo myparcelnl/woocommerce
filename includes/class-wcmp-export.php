7c6cfb158ea107d09f0fc176b600c54c884ee172
Richard Perdaan <richardperdaan@live.nl>
2018-03-22 15:41:29 +0100

    delete channel_engine code
    Because this code conflicts, I delete this code to make a new version. This codes will be made again, but without there are being any errors.

--- includes/class-wcmp-export.php
+++ includes/class-wcmp-export.php
@@ -10,8 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( !class_exists( 'WooCommerce_MyParcel_Export' ) ) :
 
 class WooCommerce_MyParcel_Export {
-	const REGEX_SPLIT_STREET = '~(?P<street>.*?)\s?(?P<street_suffix>(?P<number>[\d]+)[\s-]{0,2}(?P<number_suffix>[a-zA-Z/\s]{0,5}$|[0-9/]{0,5}$|\s[a-zA-Z]{1}[0-9]{0,3}$|\s[0-9]{2}[a-zA-Z]{0,3}$))$~';
-
 	public $order_id;
 	public $success;
 	public $errors;
@@ -552,10 +550,6 @@ class WooCommerce_MyParcel_Export {
 
 		);
 
-		$this->channel_engine_myparcel_check($order);
-		$this->split_street_admin($order);
-
-
 		$shipping_country = WCX_Order::get_prop( $order, 'shipping_country' );
 		if ( $shipping_country == 'NL' ) {
 			// use billing address if old 'pakjegemak' (1.5.6 and older)
@@ -591,52 +585,6 @@ class WooCommerce_MyParcel_Export {
 		$address = array_merge( $address, $address_intl);
 
 		return apply_filters( 'wc_myparcel_recipient', $address, $order );
-	}
-	
-	/**
-	 * When the ChannelEngine plugin is activated, copy the barcode and place them in the ChannelEngine - Track & Trace field
-	 */
-	private function channel_engine_myparcel_check($order){
-		if (!empty(WCX_Order::get_meta( $order, '_shipping_ce_track_and_trace' ))) {
-			return;
-		}
-
-		$order_shipments = WCX_Order::get_meta( $order, '_myparcel_shipments' );
-
-		if (!empty($order_shipments)) {
-
-			$keys = array_keys( $order_shipments );
-			$order_number = $keys[0];
-
-			$tracking = $order_shipments[ $order_number ][ 'tracktrace' ]; // auto
-			update_post_meta( $order->id, '_shipping_ce_track_and_trace', $tracking );
-		}
-
-		return;
-	}
-
-	/**
-	 *
-	 * Get the street from _shipping_address_1 and split them in street, street_suffix, number and number_suffix
-	 * Place the street, street_suffix, number and number_suffix into the correct text field (for export)
-	 *
-	 * @param $order
-	 */
-	private function split_street_admin($order){
-		$split_street_regex = self::REGEX_SPLIT_STREET;
-		$address = trim((string) WCX_Order::get_prop( $order, 'shipping_address_1' ));
-		preg_match($split_street_regex, $address, $matches);
-
-		if (empty (WCX_Order::get_meta( $order, '_shipping_street_name' ) && WCX_Order::get_meta( $order, '_shipping_house_number' ))){
-
-			update_post_meta( $order->id, '_shipping_street_name', $matches['street'] );
-			update_post_meta( $order->id, '_shipping_house_number', $matches['number'] );
-
-			if (empty(WCX_Order::get_meta( $order, '_shipping_house_number_suffix' ))) {
-				update_post_meta( $order->id, '_shipping_house_number_suffix', (string) WCX_Order::get_prop( $order, 'shipping_address_2' ) );
-			}
-		}
-		return;
 	}
 
 	public function get_options( $order ) {
