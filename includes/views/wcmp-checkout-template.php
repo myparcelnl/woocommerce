572451774ca810d9b30efff0ed29474e93fe1233
Richard Perdaan <richardperdaan@live.nl>
2018-04-16 21:22:28 +0200

    remove al the NL option inside the checkout

--- includes/views/wcmp-checkout-template.php
+++ includes/views/wcmp-checkout-template.php
@@ -12,11 +12,10 @@
 	?>
 </style>
 
-<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-
 <div id="mypa-spinner"></div>
 <div id="mypa-message"></div>
 <div id="mypa-location-details"></div>
+
 
 <div id="mypa-delivery-option-form">
     <fieldset>
@@ -50,66 +49,6 @@
                 </div>
             </li>
         </ul>
-        <ul id="mypa-pre-selectors-nl">
-            <li>
-                <label for="method-myparcel-signature-selector">Handtekening</label>
-                <div><input name="myparcel-signature-selector" id="mypa-signature-selector" type="checkbox" value="myparcel-signature-selector" ></div>
-            </li>
-            <li>
-                <label for="method-myparcel-recipient-only-selector">Alleen geadresseerde</label>
-                <div><input name="method-yparcel-recipient-only-selector" id="mypa-recipient-only-selector" type="checkbox" value="myparcel-recipient-only-selector" ></div>
-                <a id="mypa-method-myparcel-recipient-only-selector-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-        </ul>
-        <ul id="mypa-delivery-selectors-nl">
-            <li class="mypa-delivery-option method-myparcel-flatrate-div">
-                <label for="method-myparcel-flatrate">Thuislevering.</label>
-                <div><input name="shipping-method" id="method-myparcel-flatrate" type="radio" value="myparcel-flatrate"></div>
-                <a id="mypa-method-myparcel-flatrate-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-delivery-signature-div">
-                <label for="method-myparcel-delivery-signature">Thuis of op werk bezorgd met handtekening voor ontvangst.</label>
-                <div><input name="shipping-method" id="method-myparcel-delivery-signature" type="radio" value="myparcel-delivery-signature" ></div>
-                <a id="mypa-method-myparcel-delivery-signature-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-delivery-only-recipient-div">
-                <label for="method-myparcel-delivery-only-recipient">Thuis of op werk bezorgd, niet bij de buren.</label>
-                <div><input name="shipping-method" id="method-myparcel-delivery-only-recipient" type="radio" value="myparcel-delivery-only-recipient" ></div>
-                <a id="mypa-method-myparcel-delivery-only-recipient-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-delivery-signature-and-only-recipient-fee-div">
-                <label for="method-myparcel-delivery-signature-and-only-recipient-fee">Thuis of op werk bezorgd, niet bij de buren, met handtekening.</label>
-                <div><input name="shipping-method" id="method-myparcel-delivery-signature-and-only-recipient-fee" type="radio" value="myparcel-delivery-signature-and-only-recipient-fee" ></div>
-                <a id="mypa-method-myparcel-delivery-signature-and-only-recipient-fee-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-delivery-evening-signature-div">
-                <label for="method-myparcel-delivery-evening-signature">Avondlevering met handtekening.</label>
-                <div><input name="shipping-method" id="method-myparcel-delivery-evening-signature" type="radio" value="myparcel-delivery-evening-signature" ></div>
-                <a id="mypa-method-myparcel-delivery-evening-signature-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-pickup-express-div">
-                <label for="method-myparcel-pickup-express">Extra vroeg ophalen bij PostNL.</label>
-                <div><input name="shipping-method" id="method-myparcel-pickup-express" type="radio" value="myparcel-pickup-express" ></div>
-                <a id="mypa-method-myparcel-deliverypickup-expreshelp" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-morning-div">
-                <label for="method-myparcel-morning">Ochtendlevering.</label>
-                <div><input name="shipping-method" id="method-myparcel-morning" type="radio" value="myparcel-morning" ></div>
-                <a id="mypa-method-myparcel-morning-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-
-            <li class="mypa-delivery-option method-myparcel-morning-signature-div">
-                <label for="method-myparcel-morning-signature">Ochtendlevering met handtekening.</label>
-                <div><input name="shipping-method" id="method-myparcel-morning-signature" type="radio" value="myparcel-morning-signature" ></div>
-                <a id="mypa-method-myparcel-morning-signature-help" class="mypa-help" href=""><i class="fas fa-question"></i></a>
-            </li>
-        </ul>
         <ul id="mypa-pickup-location-selector">
             <li>
                 <label for="mypa-pickup-location">Afhalen op locatie<span id="mypa-price-pickup"></span></label>
@@ -118,7 +57,9 @@
                     <select name="mypa-pickup-location" id="mypa-pickup-location">
                         <option value="">Geen Locatie</option>
                     </select>
-                    <span id="mypa-show-location-details"><i class="fas fa-clock"></i></span>
+                    <span id="mypa-show-location-details">
+                        <svg class="svg-inline--fa fa-clock fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="clock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm57.1 350.1L224.9 294c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h48c6.6 0 12 5.4 12 12v137.7l63.5 46.2c5.4 3.9 6.5 11.4 2.6 16.8l-28.2 38.8c-3.9 5.3-11.4 6.5-16.8 2.6z"></path></svg>
+                    </span>
                 </div>
             </li>
         </ul>
