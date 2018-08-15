759b50530f259f7ba3d01ee3917df83429e41fc9
Adem Demir <adem@myparcel.nl>
2018-08-15 09:13:28 +0200

    Shipping method
    Fix: if there is one shipping method is selected

--- includes/class-wcmp-export.php
+++ includes/class-wcmp-export.php
@@ -1438,12 +1438,15 @@ if ( !class_exists( 'WooCommerce_PostNL_Export' ) ) :
          */
         private function isActiveMethod( $shipping_method_id, $package_type_shipping_methods, $shipping_method_id_class, $shipping_class ) {
 
-            //support WooCommerce flate rate
+            // support WooCommerce flate rate
             // check if we have a match with the predefined methods
+            if (in_array($shipping_method_id, $package_type_shipping_methods)) {
+                return true;
+            }
             if (in_array($shipping_method_id_class, $package_type_shipping_methods)) {
                 return true;
             }
-            //support WooCommerce Table Rate Shipping by Automattic
+            // support WooCommerce Table Rate Shipping by Automattic
             // fallback to bare method (without class) (if bare method also defined in settings)
             if (!empty($shipping_class) && in_array($shipping_class, $package_type_shipping_methods)) {
                 return true;
