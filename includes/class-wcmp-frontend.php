d6331094d71c8f0028950ab3a20444e5872a6f95
Richard Perdaan <richard@myparcel.nl>
2018-10-11 10:14:33 +0200

    set wpo_wcpdf_delivery_options code back

--- includes/class-wcmp-frontend.php
+++ includes/class-wcmp-frontend.php
@@ -42,8 +42,10 @@ if ( !class_exists( 'WooCommerce_MyParcel_Frontend' ) ) :
             add_action( 'woocommerce_thankyou', array( $this, 'thankyou_pickup_html'), 10, 1 );
 
             // WooCommerce PDF Invoices & Packing Slips Premium Templates compatibility
+            add_filter( 'wpo_wcpdf_templates_replace_myparcel_delivery_date', array( $this, 'wpo_wcpdf_delivery_date' ), 10, 2 );
             add_filter( 'wpo_wcpdf_templates_replace_myparcel_tracktrace', array( $this, 'wpo_wcpdf_tracktrace' ), 10, 2 );
             add_filter( 'wpo_wcpdf_templates_replace_myparcel_tracktrace_link', array( $this, 'wpo_wcpdf_tracktrace_link' ), 10, 2 );
+            add_filter( 'wpo_wcpdf_templates_replace_myparcel_delivery_options', array( $this, 'wpo_wcpdf_delivery_options' ), 10, 2 );
 
             // Delivery options
             if (isset(WooCommerce_MyParcel()->checkout_settings['myparcel_checkout'])) {
@@ -113,6 +115,20 @@ if ( !class_exists( 'WooCommerce_MyParcel_Frontend' ) ) :
             }
 
             return $actions;
+        }
+
+        public function wpo_wcpdf_delivery_options( $replacement, $order ) {
+            ob_start();
+            WooCommerce_MyParcel()->admin->show_order_delivery_options( $order );
+            return ob_get_clean();
+        }
+
+        public function wpo_wcpdf_delivery_date( $replacement, $order ) {
+            if ($delivery_date = WooCommerce_MyParcel()->export->get_delivery_date( $order ) ) {
+                $formatted_date = date_i18n( apply_filters( 'wcmyparcel_delivery_date_format', wc_date_format() ), strtotime( $delivery_date ) );
+                return $formatted_date;
+            }
+            return $replacement;
         }
 
         public function wpo_wcpdf_tracktrace( $replacement, $order ) {
