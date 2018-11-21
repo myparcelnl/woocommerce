4bcb879472379077b9e578c6fb028bdce75caf89
Edie Lemoine <edie@myparcel.nl>
2018-11-21 16:07:53 +0100

    update wcmp-checkout fields to check if myparcel.js is loaded and refactor a bit

--- assets/js/wcmp-checkout-fields.js
+++ assets/js/wcmp-checkout-fields.js
@@ -5,18 +5,28 @@ jQuery(function($) {
 
     // Auto fill helper for layout with separate number field
     // Split street to 3 fields on autofill
-    jQuery('#billing_street_name, #shipping_street_name').on('animationend', function() {
-        type = jQuery(this).attr('id').search('billing') ? 'shipping' : 'billing';
-        jQuery('#' + type + '_address_1').val(this.value); // Fill in the hidden address line 1 field in case a theme forces it to be required
-        address = this.value.split(
+    $('#billing_street_name, #shipping_street_name').on('load, animationend', function() {
+        setAddress(this);
+    });
+
+    function setAddress(el) {
+        type = $(el).attr('id').search('billing') ? 'shipping' : 'billing';
+
+        $('#' + type + '_address_1').val(el.value); // Fill in the hidden address line 1 field in case a theme forces it to be required
+        address = el.value.split(
             /(.*?)\s?(\d{1,4})[/\s\-]{0,2}([a-zA-Z]{1}\d{1,3}|-\d{1,4}|\d{2}\w{1,2}|[a-zA-Z]{1}[a-zA-Z\s]{0,3})?$/g
         )
             .filter(function(value) {
                 return value !== ''
             }); // filter out empty values
 
-        jQuery('#' + type + '_street_name').val(address[0]);
-        jQuery('#' + type + '_house_number').val(address[1]);
-        jQuery('#' + type + '_house_number_suffix').val(address[2]);
-    });
+        $('#' + type + '_street_name').val(address[0]);
+        $('#' + type + '_house_number').val(address[1]);
+        $('#' + type + '_house_number_suffix').val(address[2]);
+
+        // Update delivery options after filling if myparcel.js is loaded and initialized
+        if (typeof MyParcel != 'undefined' && MyParcel.data.length === 0) {
+            MyParcel.callDeliveryOptions();
+        }
+    }
 });
