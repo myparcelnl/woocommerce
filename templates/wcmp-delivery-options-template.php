75282e32b82fcafc31becf76cf7da24c3cc8882a
Richard Perdaan <richard@myparcel.nl>
2018-12-20 13:21:37 +0100

    change logo's to bpost logo

--- templates/wcmp-delivery-options-template.php
+++ templates/wcmp-delivery-options-template.php
@@ -10,7 +10,7 @@
 
     <div id="mypabe-spinner-model">
         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 99" enable-background="new 0 0 100 99" xml:space="preserve">
-            <image id="postnl-logo" width="100" height="99" href="<?php echo WooCommerce_MyParcelBE()->plugin_url() . '/assets/img/wcmp-postnl-logo.png' ?>" />
+            <image id="bpost-logo" width="100" height="99" href="<?php echo WooCommerce_MyParcelBE()->plugin_url() . '/assets/img/wcmp-bpost-logo.png' ?>" />
         </svg>
         <div id="mypabe-spinner"></div>
     </div>
