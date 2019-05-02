0da71ab94252aa61f8a1ab78e57ab46b6eb39130
Adem Demir <37372250+adem-demir@users.noreply.github.com>
2019-05-02 13:31:49 +0200

    Insurance in admin changed to 1 option (#63)
    Insurance in admin changed to 1 option it was  showing 3 options. But this isn't possible by Bpost

--- includes/class-wcmp-settings.php
+++ includes/class-wcmp-settings.php
@@ -447,40 +447,6 @@ class WooCommerce_MyParcelBE_Settings {
         );
 
         add_settings_field(
-			'insured_amount',
-            __('Insured amount', 'woocommerce-myparcelbe'),
-            array($this->callbacks, 'select'),
-            $option_group,
-            'defaults',
-			array(
-                'option_name' => $option_name,
-                'id'          => 'insured_amount',
-                'default'     => 'standard',
-                'class'       => 'insured_amount',
-                'options'     => array(
-                    '49'  => __('Insured up to &euro; 50 (+ &euro; 0.50)', 'woocommerce-myparcelbe'),
-                    '249' => __('Insured up to &euro; 250 (+ &euro; 1.00)', 'woocommerce-myparcelbe'),
-                    '499' => __('Insured up to &euro; 500 (+ &euro; 1.65)', 'woocommerce-myparcelbe'),
-                    ''    => __('> &euro; 500 insured (+ &euro; 1.65 / &euro; 500)', 'woocommerce-myparcelbe'),
-                ),
-            )
-        );
-
-        add_settings_field(
-            'insured_amount_custom',
-            __('Insured amount (in euro)', 'woocommerce-myparcelbe'),
-            array($this->callbacks, 'text_input'),
-            $option_group,
-            'defaults',
-			array(
-                'option_name' => $option_name,
-                'id'          => 'insured_amount_custom',
-                'size'        => '5',
-                'class'       => 'insured_amount',
-            )
-        );
-
-        add_settings_field(
 			'label_description',
             __('Label description', 'woocommerce-myparcelbe'),
             array($this->callbacks, 'text_input'),
