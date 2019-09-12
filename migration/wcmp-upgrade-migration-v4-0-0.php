251b27113a5fbd733e5b6a3791a813af05412d91
Edie Lemoine <edie@myparcel.nl>
2019-09-12 15:44:29 +0200

    MY-13664 move the 4.0.0 migration logic

--- migration/wcmp-upgrade-migration-v4-0-0.php
+++ migration/wcmp-upgrade-migration-v4-0-0.php
@@ -13,20 +13,86 @@ if (! class_exists('wcmp_installation_migration_v4_0_0')) :
     {
         public function __construct()
         {
-            $old_settings = get_option('woocommerce_myparcelbe_checkout_settings');
-            $new_settings = $old_settings;
+           // TODO
+        }
 
-            // Rename signed to signature for consistency
-            $new_settings['signature_enabled'] = $old_settings['signed_enabled'];
-            $new_settings['signature_title']   = $old_settings['signed_title'];
-            $new_settings['signature_fee']     = $old_settings['signed_fee'];
+        /**
+         * @param array $legacyCheckoutSettings
+         * @param array $newDefaultSettings
+         *
+         * @return array
+         */
+        public function migrateDeliveryOptionsSettings(array $legacyCheckoutSettings, array $newDefaultSettings)
+        {
+            $bpostSettings         = $legacyCheckoutSettings;
+            $singleCarrierDefaults = $newDefaultSettings;
 
-            // Remove old settings
-            unset($new_settings['signed_enabled']);
-            unset($new_settings['signed_title']);
-            unset($new_settings['signed_fee']);
+            $bpostSettings         = $this->setFromCheckoutToBpostSettings($bpostSettings, $legacyCheckoutSettings);
+            $singleCarrierDefaults = $this->setFromDefaultToBpostSettings($singleCarrierDefaults, $newDefaultSettings);
 
-            update_option('woocommerce_myparcelbe_checkout_settings', $new_settings);
+            return [$bpostSettings, $singleCarrierDefaults];
+        }
+
+        public function setFromDefaultToBpostSettings($bpostSettings, $defaultSettings)
+        {
+            $fromDefaultToBpost = [
+                "signature" => "bpost_signature",
+                "insured"   => "insured",
+            ];
+
+            foreach ($fromDefaultToBpost as $carrierSettings) {
+                $bpostSettings[$carrierSettings] = $defaultSettings[$carrierSettings];
+            }
+
+            return $bpostSettings;
+        }
+
+        /**
+         * @param array $checkoutSettings
+         * @param array $generalSettings
+         *
+         * @return array
+         */
+        public function setFromCheckoutToGeneralSettings(array $checkoutSettings, array $generalSettings)
+        {
+            $fromCheckoutToGeneral = [
+                "use_split_address_fields"      => "use_split_address_fields",
+                "delivery_options_display"      => "delivery_options_display",
+                "checkout_position"             => "delivery_options_position",
+                "header_delivery_options_title" => "header_delivery_options_title",
+                "customs_css"                   => "header_delivery_options_title",
+                "myparcelbe_checkout"           => "delivery_options_enabled",
+            ];
+
+            foreach ($fromCheckoutToGeneral as $generalCarrierSettings) {
+                $generalSettings[$generalCarrierSettings] = $checkoutSettings[$generalCarrierSettings];
+            }
+
+            return $generalSettings;
+        }
+
+        public function setFromCheckoutToBpostSettings($bpostSettings, $checkoutSettings)
+        {
+            $fromCheckoutToBpost = [
+                'dropoff_days'        => 'bpost_drop_off_days',
+                'cutoff_time'         => 'bpost_cutoff_time',
+                'dropoff_delay'       => 'bpost_drop_off_delay',
+                'deliverydays_window' => 'bpost_delivery_days_window',
+                'signature_enabled'   => 'bpost_signature_enabled',
+                'signature_title'     => 'bpost_signature_title',
+                'signature_fee'       => 'bpost_signature_fee',
+                'delivery_enabled'    => 'bpost_delivery_enabled',
+                'pickup_enabled'      => 'bpost_pickup_enabled',
+                'pickup_title'        => 'bpost_pickup_title',
+                'pickup_fee'          => 'bpost_pickup_fee',
+            ];
+
+            foreach ($fromCheckoutToBpost as $singleCarrierSettings => $multiCarrierSettings) {
+                $bpostSettings[$multiCarrierSettings] = $checkoutSettings[$singleCarrierSettings];
+                unset($bpostSettings[$singleCarrierSettings]);
+            }
+
+            return $bpostSettings;
         }
     }
 
