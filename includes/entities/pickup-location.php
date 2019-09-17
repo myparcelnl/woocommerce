106a9eb0d106000769a9dcec0a7dd32ed7c4617f
Edie Lemoine <edie@myparcel.nl>
2019-09-17 11:16:02 +0200

    make PickupLocation class

---
+++ includes/entities/pickup-location.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace WPO\WC\MyParcelBE\Entity;
+
+/**
+ * Class PickupLocation
+ *
+ * @package WPO\WC\MyParcelBE\Entity
+ */
+class PickupLocation
+{
+    /**
+     * @var string
+     */
+    private $location_name;
+
+    /**
+     * @var string
+     */
+    private $street;
+
+    /**
+     * @var string
+     */
+    private $number;
+
+    /**
+     * @var string
+     */
+    private $postal_code;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    public function __construct(array $data)
+    {
+        $this->location_name = $data["location_name"];
+        $this->street        = $data["street"];
+        $this->number        = $data["number"];
+        $this->postal_code   = $data["postal_code"];
+        $this->city          = $data["city"];
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocationName(): string
+    {
+        return $this->location_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode(): string
+    {
+        return $this->postal_code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+}
