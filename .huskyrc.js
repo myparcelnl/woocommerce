2411dd8c9292a431bb84e0afc1ef0affce279998
Edie Lemoine <edie@myparcel.nl>
2019-09-18 11:00:46 +0200

    update .huskyrc.js

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,12 +1,10 @@
 const packageName = '@myparcel/checkout';
 
 const preCommit = [
-  `npm i ${packageName}@latest`,
-  `cp node_modules/${packageName}/dist/myparcel.js assets/js`,
+  `npm update ${packageName}`,
+  `npm run postinstall`,
   'git add package-lock.json',
   'git add assets/js/myparcel.js',
-  'git commit -m "Updated delivery options"',
-  'git push --no-verify',
 ].join(' && ');
 
 module.exports = {
