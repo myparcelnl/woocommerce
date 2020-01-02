f59d4ea1c229c4947683b1c12812cf763c383fcf
Richard Perdaan <richard@myparcel.nl>
2020-01-02 11:03:36 +0100

    use setOnlyRecipient

--- includes/admin/class-wcmp-export-consignments.php
+++ includes/admin/class-wcmp-export-consignments.php
@@ -174,6 +174,17 @@ class WCMP_Export_Consignments
     /**
      * @return bool
      */
+    private function getOnlyRecipient(): bool
+    {
+        return WCMP_Export::getChosenOrDefaultShipmentOption(
+            $this->deliveryOptions->getShipmentOptions()->hasOnlyRecipient(),
+            "{$this->carrier}_" . WCMP_Settings::SETTING_CARRIER_DEFAULT_EXPORT_ONLY_RECIPIENT
+        );
+    }
+
+    /**
+     * @return bool
+     */
     private function getAgeCheck(): bool
     {
         return WCMP_Export::getChosenOrDefaultShipmentOption(
@@ -212,7 +223,6 @@ class WCMP_Export_Consignments
 
         return 0;
     }
-
 
     /**
      * Gets the recipient and puts its data in the consignment.
@@ -299,6 +309,7 @@ class WCMP_Export_Consignments
     {
         $this->consignment
             ->setSignature($this->getSignature())
+            ->setOnlyRecipient($this->getOnlyRecipient())
             ->setInsurance($this->getInsurance())
             ->setAgeCheck($this->getAgeCheck());
     }
