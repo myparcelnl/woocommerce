c875b86345c9e85538c9be74129cf586f999b972
Robin de Laater <me@robindelaater.nl>
2020-03-06 14:57:19 +0100

    Revert capitalized letters to lowercase

--- includes/admin/class-wcmp-api.php
+++ includes/admin/class-wcmp-api.php
@@ -153,7 +153,7 @@ class WCMP_API extends WCMP_Rest
         $collection = MyParcelCollection::findMany($shipment_ids, $this->key);
 
         /**
-         * @see https://github.com/MyParcelNL/Sdk#label-format-and-position
+         * @see https://github.com/myparcelnl/sdk#label-format-and-position
          */
         if (WCMP()->setting_collection->getByName(WCMP_Settings::SETTING_LABEL_FORMAT) === "A6") {
             $positions = false;
