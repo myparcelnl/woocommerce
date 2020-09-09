e36e8bc0e002962d746d992afa340c1e744222b0
Richard Perdaan <richard@myparcel.nl>
2020-09-09 09:58:59 +0200

    useragent format

--- includes/admin/class-wcmp-export-consignments.php
+++ includes/admin/class-wcmp-export-consignments.php
@@ -70,10 +70,9 @@ class WCMP_Export_Consignments
         $this->deliveryOptions    = WCMP_Admin::getDeliveryOptionsFromOrder($order);
         $this->carrier            = $this->deliveryOptions->getCarrier() ?? WCMP_Data::DEFAULT_CARRIER;
         $this->myParcelCollection = (new MyParcelCollection())->setUserAgent(
-            "Wordpress/" . get_bloginfo("version") .
-            ' WooCommerce/' . WOOCOMMERCE_VERSION,
-            ' MyParcel-WooCommerce/' . WC_MYPARCEL_NL_VERSION);
-
+            "Wordpress", get_bloginfo("version").
+            ' WooCommerce/' . WOOCOMMERCE_VERSION .
+            ' MyParcelNL-WooCommerce/' . WC_MYPARCEL_NL_VERSION);
 
         $this->createConsignment();
         $this->setConsignmentData();
