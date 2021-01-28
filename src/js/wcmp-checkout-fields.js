fbf2c5cc9678707e0652aa5cf14f074e07a87bbb
Edie Lemoine <edie@myparcel.nl>
2021-01-28 15:58:54 +0100

    fix: error when forcing shipping address as billing address

--- src/js/wcmp-checkout-fields.js
+++ src/js/wcmp-checkout-fields.js
@@ -3,11 +3,11 @@
  *
  * Compatible with 5.6 and 7.1 version of the plugin.
  */
-window.addEventListener('load', function() {
-  var addressRegex = /(.*?)\s?(\d{1,4})[/\s-]{0,2}([a-zA-Z]\d{1,3}|-\d{1,4}|\d{2}\w{1,2}|[a-zA-Z][a-zA-Z\s]{0,3})?$/g;
+window.addEventListener('load', () => {
+  const addressRegex = /(.*?)\s?(\d{1,4})[/\s-]{0,2}([a-zA-Z]\d{1,3}|-\d{1,4}|\d{2}\w{1,2}|[a-zA-Z][a-zA-Z\s]{0,3})?$/g;
 
-  var billingStreet = document.querySelector('#billing_street_name');
-  var shippingStreet = document.querySelector('#shipping_street_name');
+  const billingStreet = document.querySelector('#billing_street_name');
+  const shippingStreet = document.querySelector('#shipping_street_name');
 
   /**
    * Trigger hiding of address line 1 and 2 if necessary. The timeout is necessary, otherwise the order summary is going
@@ -16,48 +16,55 @@ window.addEventListener('load', function() {
    * It checks for MyParcel_Frontend before running as that script has the same code.
    */
   if (!window.hasOwnProperty('MyParcel_Frontend')) {
-    setTimeout(function() {
-      var event = document.createEvent('HTMLEvents');
+    setTimeout(() => {
+      const event = document.createEvent('HTMLEvents');
       event.initEvent('change', true, false);
-      document.querySelectorAll('.country_to_state').forEach(function(selector) {
-        selector.dispatchEvent(event);
-      });
+
+      document
+        .querySelectorAll('.country_to_state')
+        .forEach((selector) => {
+          selector.dispatchEvent(event);
+        });
     }, 100);
   }
 
   /**
    * Set the correct autocomplete attribute on the street fields if none is present.
    */
-  [billingStreet, shippingStreet].forEach(function(el) {
-    if (!el.getAttribute('autocomplete')) {
-      el.setAttribute('autocomplete', 'street-address');
+  [billingStreet, shippingStreet].forEach((element) => {
+    if (!element) {
+      return;
     }
+
+    if (!element.getAttribute('autocomplete')) {
+      element.setAttribute('autocomplete', 'street-address');
+    }
+
+    element.addEventListener('load', setAddress);
+    element.addEventListener('animationend', setAddress);
   });
-
-  billingStreet.addEventListener('load', setAddress);
-  billingStreet.addEventListener('animationend', setAddress);
-
-  shippingStreet.addEventListener('load', setAddress);
-  shippingStreet.addEventListener('animationend', setAddress);
 
   /**
    * Autofill helper for layout with separate number field.
    * Split street to 3 fields on autofill.
+   *
+   * @param {Event} event
    */
-  function setAddress() {
-    var type = this.getAttribute('id').search('billing') ? 'shipping' : 'billing';
+  function setAddress(event) {
+    const element = event.target;
+    const type = element.getAttribute('id').search('billing') ? 'shipping' : 'billing';
 
     /* filter out empty values */
-    var address = this.value.split(addressRegex).filter(function(value) {
-      return !!value;
-    });
+    const address = element.value.split(addressRegex).filter((value) => !!value);
 
     /* Fill in the hidden address line 1 field in case a theme forces it to be required */
-    document.querySelector('#' + type + '_address_1').value = this.value;
+    document.querySelector(`#${type}_address_1`).value = element.value;
 
-    document.querySelector('#' + type + '_street_name').value = address[0] || '';
-    document.querySelector('#' + type + '_house_number').value = address[1] || '';
-    document.querySelector('#' + type + '_house_number_suffix').value = address[2] || '';
+    const [street, number, suffix] = address;
+
+    document.querySelector(`#${type}_street_name`).value = street || '';
+    document.querySelector(`#${type}_house_number`).value = number || '';
+    document.querySelector(`#${type}_house_number_suffix`).value = suffix || '';
 
     /* Update settings after filling if the frontend script is loaded and initialized and address is not empty */
     if (address.length && window.hasOwnProperty('MyParcel_Frontend')) {
