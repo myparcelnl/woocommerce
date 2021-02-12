7c8da6bd70967dc8fe7e8981276241180c4c789b
Adem Demir <37372250+adem-demir@users.noreply.github.com>
2021-02-12 10:26:06 +0100

    fix: url for old vendor map

--- migration/wcmp-upgrade-migration-v4-1-0.php
+++ migration/wcmp-upgrade-migration-v4-1-0.php
@@ -57,7 +57,7 @@ class WCMP_Upgrade_Migration_v4_1_0 extends WCMP_Upgrade_Migration
 
     protected function import(): void
     {
-        require_once(WCMYPA()->plugin_path() . '/includes/vendor/autoload.php');
+        require_once(WCMYPA()->plugin_path() . '/vendor/autoload.php');
         require_once(WCMYPA()->plugin_path() . '/includes/admin/settings/class-wcmypa-settings.php');
         require_once(WCMYPA()->plugin_path() . '/includes/class-wcmp-data.php');
     }
