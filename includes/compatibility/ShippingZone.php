f7d5d0fe65de2c2bd348465c329d9920e91a247f
Joey de Vries <joey.devries@myparcel.nl>
2021-10-01 09:42:18 +0200

    refactor: pr reformat feedback

--- includes/compatibility/ShippingZone.php
+++ includes/compatibility/ShippingZone.php
@@ -16,20 +16,21 @@ class ShippingZone extends WC_Shipping_Zone
      *
      * @param  int|object|array $zone Zone ID to load from the DB or zone object.
      */
-    public function __construct( $zone = null ) {
-        if ( is_numeric( $zone ) && ! empty( $zone ) ) {
-            $this->set_id( $zone );
+    public function __construct($zone = null)
+    {
+        if (is_numeric($zone) && ! empty($zone)) {
+            $this->set_id($zone);
             $this->call_wc_shipping_zone();
-        } elseif ( is_object( $zone ) ) {
-            $this->set_id( $zone->zone_id );
+        } elseif (is_object($zone)) {
+            $this->set_id($zone->zone_id);
             $this->call_wc_shipping_zone();
-        } elseif ( is_array($zone) ) {
+        } elseif (is_array($zone)) {
             $this->transformToObject($zone);
-        } elseif ( 0 === $zone || '0' === $zone ) {
-            $this->set_id( 0 );
+        } elseif (0 === $zone || '0' === $zone) {
+            $this->set_id(0);
             $this->call_wc_shipping_zone();
         } else {
-            $this->set_object_read( true );
+            $this->set_object_read(true);
         }
     }
 
@@ -41,11 +42,11 @@ class ShippingZone extends WC_Shipping_Zone
         $this->set_id($zoneData['id']);
         $this->set_zone_name($zoneData['zone_name']);
         $this->set_zone_order($zoneData['zone_order']);
-        if( $zoneData['zone_locations'] ) {
+        if ($zoneData['zone_locations']) {
             $this->set_zone_locations($zoneData['zone_locations']);
         }
         $this->set_meta_data($zoneData['meta_data']);
-        $this->set_object_read( true );
+        $this->set_object_read(true);
     }
 
     /**
@@ -54,9 +55,9 @@ class ShippingZone extends WC_Shipping_Zone
      */
     private function call_wc_shipping_zone(): void
     {
-        $this->data_store = WC_Data_Store::load( 'shipping-zone' );
-        if ( false === $this->get_object_read() ) {
-            $this->data_store->read( $this );
+        $this->data_store = WC_Data_Store::load('shipping-zone');
+        if (false === $this->get_object_read()) {
+            $this->data_store->read($this);
         }
     }
 }
