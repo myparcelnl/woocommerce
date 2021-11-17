bf2d29a9db63ca42dfef23f5c92f5e1e55b62864
Edie Lemoine <edie@myparcel.nl>
2021-11-17 15:29:48 +0100

    chore: update to 4.7.0

--- woocommerce-myparcel.php
+++ woocommerce-myparcel.php
@@ -5,7 +5,7 @@ Plugin URI: https://myparcel.nl/
 Description: Export your WooCommerce orders to MyParcel (https://myparcel.nl/) and print labels directly from the WooCommerce admin
 Author: MyParcel
 Author URI: https://myparcel.nl
-Version: 4.6.0
+Version: 4.7.0
 Text Domain: woocommerce-myparcel
 
 License: GPLv3 or later
@@ -37,7 +37,7 @@ if (! class_exists('WCMYPA')) :
         /**
          * @var string
          */
-        public $version = '4.6.0';
+        public $version = '4.7.0';
 
         /**
          * @var string
