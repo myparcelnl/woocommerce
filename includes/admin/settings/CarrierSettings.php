74f03a2c9ae2000c3a6c3d70d33b85a6a13d69c7
markernst97 <markiervc@gmail.com>
2022-02-17 11:41:00 +0100

    refactor: add own function for getting retail overview link

--- includes/admin/settings/CarrierSettings.php
+++ includes/admin/settings/CarrierSettings.php
@@ -73,7 +73,7 @@ class CarrierSettings
         if (! $dropOffPoint) {
             return WCMP_Settings_Callbacks::getLink(
                 __('diagnostics_status_drop_off_point_missing', 'woocommerce-myparcel'),
-                Status::LINK_RETAIL_OVERVIEW . "?carrier={$carrier->getId()}"
+                self::getRetailOverviewLink($carrier)
             );
         }
 
@@ -86,9 +86,19 @@ class CarrierSettings
             $dropOffPoint->getCity(),
             WCMP_Settings_Callbacks::getLink(
                 __('diagnostics_status_drop_off_point_manage', 'woocommerce-myparcel'),
-                Status::LINK_RETAIL_OVERVIEW . "?carrier={$carrier->getId()}"
+                self::getRetailOverviewLink($carrier)
             )
         );
+    }
+
+    /**
+     * @param  \MyParcelNL\Sdk\src\Model\Carrier\AbstractCarrier $carrier
+     *
+     * @return string
+     */
+    public static function getRetailOverviewLink(AbstractCarrier $carrier): string
+    {
+        return Status::LINK_RETAIL_OVERVIEW . "?carrier={$carrier->getId()}";
     }
 
     /**
