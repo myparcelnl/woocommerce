81928225dc3a3ad778874853be0e267f947496d0
Edie Lemoine <edie@myparcel.nl>
2022-06-20 14:42:13 +0200

    build: update push workflow

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 const mainConfig = require('@myparcel/semantic-release-config');
-const {addExecPlugin, addGitHubPlugin, addGitPlugin} = require('@myparcel/semantic-release-config/src/plugins');
+const { addExecPlugin, addGitHubPlugin, addGitPlugin } = require('@myparcel/semantic-release-config/src/plugins');
+const { gitPluginDefaults } = require('@myparcel/semantic-release-config/src/plugins/addGitPlugin');
 
 module.exports = {
   ...mainConfig,
@@ -8,12 +9,19 @@ module.exports = {
     ...mainConfig.plugins,
     addGitHubPlugin({
       assets: [
-        {path: './woocommerce-myparcel.zip', label: 'Download Woocommerce MyParcel plugin v${nextRelease.version}'},
+        { path: './woocommerce-myparcel.zip', label: 'Download Woocommerce MyParcel plugin v${nextRelease.version}' },
       ],
     }),
     addExecPlugin({
       prepareCmd: 'node ./private/updateVersion.js ${nextRelease.version}',
     }),
-    addGitPlugin(),
+    addGitPlugin({
+      ...gitPluginDefaults,
+      assets: [
+        ...gitPluginDefaults.assets,
+        'woocommerce-myparcel.php',
+        'readme.txt',
+      ],
+    }),
   ],
 };
