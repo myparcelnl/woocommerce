77b112195e602210a3646adc87ba85f30188e463
Mark Ernst <33826576+Mark-Ernst@users.noreply.github.com>
2022-11-11 10:43:51 +0100

    feat: add modal option for myparcel orders only (#869)
    * feat(dashboard): add option to only show myparcel orders

--- includes/admin/views/MyParcelWidget.php
+++ includes/admin/views/MyParcelWidget.php
@@ -6,17 +6,30 @@ namespace MyParcelNL\WooCommerce\includes\admin\views;
 
 use MyParcelNL\Sdk\src\Model\Recipient;
 use MyParcelNL\WooCommerce\includes\admin\OrderSettings;
-use MyParcelNL\WooCommerce\includes\Concerns\HasApiKey;
 use TypeError;
+use WC_Order;
 use WCMP_Export;
 
 defined('ABSPATH') or die();
 
 class MyParcelWidget
 {
-    use HasApiKey;
+    private const DEFAULT_ORDER_AMOUNT       = 5;
+    private const DEFAULT_FETCH_ORDER_AMOUNT = 100;
 
-    private const DEFAULT_ORDER_AMOUNT = 5;
+    /**
+     * @return void
+     * @noinspection PhpUnused
+     */
+    public function addStyles(): void
+    {
+        $directory = explode('/includes', plugin_dir_url(__FILE__))[0] ?? null;
+        $screen    = get_current_screen();
+
+        if ($screen && $directory && 'dashboard' === $screen->id) {
+            wp_enqueue_style('myparcel_admin_dashboard_widget_style', "$directory/assets/css/widget.css", [], '1.0');
+        }
+    }
 
     /**
      * @return void
@@ -34,37 +47,62 @@ class MyParcelWidget
     }
 
     /**
+     * @return void
+     */
+    public function myparcelDashboardWidgetConfigHandler(): void
+    {
+        $options            = get_option('woocommerce_myparcel_dashboard_widget') ?: $this->getDefaultWidgetConfig();
+        $submit             = filter_input(INPUT_POST, 'submit');
+        $ordersAmount       = (int) filter_input(INPUT_POST, 'orders_amount');
+        $showMyParcelOrders = filter_input(INPUT_POST, 'showMyParcelOrders');
+
+        if (isset($submit)) {
+            if ($ordersAmount > 0) {
+                $options['items']              = $ordersAmount;
+                $options['showMyParcelOrders'] = $showMyParcelOrders;
+            }
+
+            update_option('woocommerce_myparcel_dashboard_widget', $options);
+        }
+
+        printf(
+            "
+              <p>
+                <label>%s:</label>
+                 <input class=\"form-control\" type=\"number\" min=\"1\" max=\"100\" step=\"1\" name=\"orders_amount\" value=\"%s\" />
+              </p>
+              <p>
+                <label>%s:</label>
+                <input name=\"showMyParcelOrders\" class=\"form-control\" type=\"checkbox\" %s />
+              </p>",
+            __('order_amount', 'woocommerce-myparcel'),
+            esc_attr($options['items']),
+            __('show_myparcel_orders_only', 'woocommerce-myparcel'),
+            esc_attr(isset($options['showMyParcelOrders']) ? 'checked' : '')
+        );
+    }
+
+    /**
      * @throws \Exception
      */
     public function myparcelDashboardWidgetHandler(): void
     {
-        $orderAmount = get_option('woocommerce_myparcel_dashboard_widget')['items'] ?? self::DEFAULT_ORDER_AMOUNT;
-        $orders      = wc_get_orders(
-            [
-                'limit' => $orderAmount,
-            ]
-        );
+        $orders = wc_get_orders(['limit' => self::DEFAULT_FETCH_ORDER_AMOUNT]);
 
         if (! $orders) {
             printf(esc_attr(__('no_orders_found', 'woocommerce-myparcel')));
-
             return;
         }
 
         $tableHeaders = sprintf(
-            '
-            <tr>
-              <th>%s</th>
-              <th>%s</th>
-              <th>%s</th>
-            </tr>',
-            'Order',
-            'Address',
-            'Status'
+            '<tr><th>%s</th><th>%s</th><th>%s</th></tr>',
+            __('Order', 'woocommerce-myparcel'),
+            __('Address', 'woocommerce-myparcel'),
+            __('Status', 'woocommerce-myparcel')
         );
         $tableContent = '';
 
-        foreach ($orders as $order) {
+        foreach ($this->filterOrders($orders) as $order) {
             try {
                 $orderSettings     = new OrderSettings($order);
                 $orderId           = $order->get_id();
@@ -94,57 +132,6 @@ class MyParcelWidget
     }
 
     /**
-     * @return void
-     */
-    public function addStyles(): void
-    {
-        $directory = explode('/includes', plugin_dir_url(__FILE__))[0];
-        $screen    = get_current_screen();
-        if ('dashboard' === $screen->id) {
-            wp_enqueue_style('myparcel_admin_dashboard_widget_style', $directory . '/assets/css/widget.css', [], '1.0');
-        }
-    }
-
-    /**
-     * @return void
-     */
-    public function myparcelDashboardWidgetConfigHandler(): void
-    {
-        $options      = get_option('woocommerce_myparcel_dashboard_widget') ?: $this->getDefaultWidgetConfig();
-        $submit       = filter_input(INPUT_POST, 'submit');
-        $ordersAmount = (int) filter_input(INPUT_POST, 'orders_amount');
-
-        if (isset($submit)) {
-            if ($ordersAmount > 0) {
-                $options['items'] = $ordersAmount;
-            }
-
-            update_option('woocommerce_myparcel_dashboard_widget', $options);
-        }
-
-        printf(
-            sprintf(
-                '
-              <p>
-                <label>
-                    %s:
-                </label>
-                  <input
-                    class="form-control"
-                    type="number"
-                    min="1"
-                    max="100"
-                    step="1"
-                    name="orders_amount"
-                    value="%s" />
-              </p>',
-                esc_html(__('order_amount', 'woocommerce-myparcel')),
-                esc_attr($options['items'])
-            )
-        );
-    }
-
-    /**
      * @param  null|int                                 $orderId
      * @param  null|\MyParcelNL\Sdk\src\Model\Recipient $shippingRecipient
      * @param  null|string                              $shipmentStatus
@@ -152,31 +139,56 @@ class MyParcelWidget
      * @return string
      */
     private function buildTableRow(
-        int       $orderId = null,
-        Recipient $shippingRecipient = null,
-        string    $shipmentStatus = null
+        int        $orderId = null,
+        ?Recipient $shippingRecipient = null,
+        ?string    $shipmentStatus = null
     ): string {
+        $adminUrl = get_admin_url();
+
         if (! $shippingRecipient) {
             return $this->getIncompleteTableRow($orderId);
         }
 
-        return sprintf(
-            '
-                <tr onclick="window.location=\'/wp-admin/post.php?post=%s&action=edit\';" style="cursor: pointer !important;">
-                  <td>%s</td>
-                  <td>%s %s %s %s</td>
+        return "<tr onclick=\"window.location='{$adminUrl}post.php?post={$orderId}&action=edit';\" style=\"cursor: pointer !important;\">
+                  <td>$orderId</td>
+                  <td>{$shippingRecipient->getStreet()} {$shippingRecipient->getNumber()} {$shippingRecipient->getNumberSuffix()} {$shippingRecipient->getCity()}</td>
                   <td>
-                    %s
+                    {$this->getShipmentStatusBadge($shipmentStatus)}
                   </td>
-                </tr>',
-            $orderId,
-            $orderId,
-            $shippingRecipient->getStreet(),
-            $shippingRecipient->getNumber(),
-            $shippingRecipient->getNumberSuffix(),
-            $shippingRecipient->getCity(),
-            $this->getShipmentStatusBadge($shipmentStatus)
-        );
+                </tr>";
+    }
+
+    /**
+     * @param  array $orders
+     *
+     * @return \WC_Order[]
+     * @throws \JsonException
+     */
+    private function filterOrders(array $orders): array
+    {
+        $orderAmount        = get_option('woocommerce_myparcel_dashboard_widget')['items'] ?? self::DEFAULT_ORDER_AMOUNT;
+        $showMyParcelOrders = get_option('woocommerce_myparcel_dashboard_widget')['showMyParcelOrders'];
+
+        $filteredOrders = array_filter($orders, static function ($order) use ($showMyParcelOrders) {
+            if (! $order instanceof WC_Order) {
+                return false;
+            }
+
+            $shippingClasses = $order->get_shipping_methods();
+            $orderSettings   = new OrderSettings($order);
+
+            if (! $showMyParcelOrders) {
+                return true;
+            }
+
+            if (! $shippingClasses || ! $order->get_shipping_address_1() || $orderSettings->hasLocalPickup()) {
+                return false;
+            }
+
+            return true;
+        });
+
+        return array_slice($filteredOrders, 0, $orderAmount);
     }
 
     /**
@@ -185,50 +197,9 @@ class MyParcelWidget
     private function getDefaultWidgetConfig(): array
     {
         return [
-            'items' => self::DEFAULT_ORDER_AMOUNT,
+            'items'              => self::DEFAULT_ORDER_AMOUNT,
+            'showMyParcelOrders' => true,
         ];
-    }
-
-    /**
-     * @return void
-     */
-    private function getLogoImg(): string
-    {
-        return sprintf('%s/assets/img/myparcel_logo_rgb.svg', WCMYPA()->plugin_url());
-    }
-
-    /**
-     * @param  null|string $status
-     *
-     * @return string
-     */
-    private function getShipmentStatusBadge(?string $status): string
-    {
-        return $status
-            ? sprintf('<span class="badge badge-primary">%s</span>', $status)
-            : sprintf(
-                '<span class="badge badge-secondary">%s</span>',
-                __('no_status_available', 'woocommerce-myparcel')
-            );
-    }
-
-    /**
-     * @param  null|array $shipmentIds
-     * @param             $order
-     *
-     * @return null|string
-     * @throws \Exception
-     */
-    private function getShipmentStatus(?array $shipmentIds, $order): ?string
-    {
-        if (! $shipmentIds) {
-            return null;
-        }
-
-        $firstShipmentId = $shipmentIds[0][0];
-        $shipment        = WCMYPA()->export->getShipmentData([$firstShipmentId], $order);
-
-        return $shipment ? $shipment[$firstShipmentId]['status'] : null;
     }
 
     /**
@@ -257,5 +228,52 @@ class MyParcelWidget
             $orderId,
             esc_html__('status_unknown', 'woocommerce-myparcel')
         );
+    }
+
+    /**
+     * @return void
+     */
+    private function getLogoImg(): string
+    {
+        return sprintf('%s/assets/img/myparcel_logo_rgb.svg', WCMYPA()->plugin_url());
+    }
+
+    /**
+     * @param  null|array $shipmentIds
+     * @param  \WC_Order  $order
+     *
+     * @return null|string
+     * @throws \Exception
+     */
+    private function getShipmentStatus(array $shipmentIds, WC_Order $order): ?string
+    {
+        if (empty($shipmentIds)) {
+            return null;
+        }
+
+        $firstShipmentId = $shipmentIds[0][0] ?? null;
+
+        if (! $firstShipmentId) {
+            return null;
+        }
+
+        $shipment = WCMYPA()->export->getShipmentData([$firstShipmentId], $order);
+
+        return $shipment[$firstShipmentId]['status'] ?? null;
+    }
+
+    /**
+     * @param  null|string $status
+     *
+     * @return string
+     */
+    private function getShipmentStatusBadge(?string $status): string
+    {
+        return $status
+            ? sprintf('<span class="badge badge-primary">%s</span>', $status)
+            : sprintf(
+                '<span class="badge badge-secondary">%s</span>',
+                __('no_status_available', 'woocommerce-myparcel')
+            );
     }
 }
