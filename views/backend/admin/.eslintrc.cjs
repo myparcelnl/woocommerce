7f4cab01a312061587449d2bef98598ec00233ee
Edie Lemoine <edie@myparcel.nl>
2023-01-18 16:31:48 +0100

    build: yeet annoying eslint rules

--- views/backend/admin/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: '@myparcel-eslint/eslint-config-tailwindcss',
-  settings: {
-    tailwindcss: {
-      config: 'tailwind.config.cjs',
-    },
-  },
-};
