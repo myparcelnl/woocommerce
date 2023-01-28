93c256ab7d47f3ad06ce1463324cc073de4ea83a
Edie Lemoine <edie@myparcel.nl>
2023-01-28 21:17:14 +0100

    wip

--- views/backend/admin/src/components/pdk/WcDropdownButton.vue
+++ views/backend/admin/src/components/pdk/WcDropdownButton.vue
@@ -1,38 +1,36 @@
 <template>
   <div class="mypa-flex">
     <ActionButton
-      v-for="(action, index) in standaloneActions"
+      v-for="(action, index) in dropdownActions.standalone"
       :key="action.id"
       size="sm"
       :class="{
-        '!mypa-rounded-r-none': index === standaloneActions.length - 1,
+        '!mypa-rounded-r-none': index === dropdownActions.standalone.length - 1,
       }"
       :hide-text="hideText"
       :action="action" />
 
     <PdkButton
-      v-if="dropdownActions.length > 0"
-      :aria-expanded="clickToggled || hoverToggled"
+      v-if="dropdownActions.hidden.length > 0"
+      :aria-expanded="toggled"
       :disabled="disabled"
       aria-haspopup="true"
       :aria-label="translate('toggle_dropdown')"
       class="mypa-relative"
       :class="{
-        '!mypa-rounded-l-none': standaloneActions.length > 1,
+        '!mypa-rounded-l-none': dropdownActions.hidden.length > 1,
       }"
       size="sm"
       :icon="dropdownIcon"
-      @focus="hoverToggled = true"
-      @focusout="hoverToggled = false"
-      @keydown.enter="clickToggled = !clickToggled"
-      @click="clickToggled = !clickToggled"
-      @mouseout="hoverToggled = false"
-      @mouseover="hoverToggled = true">
+      @focus="toggled = true"
+      @focusout="toggled = false"
+      @mouseout="toggled = false"
+      @mouseover="toggled = true">
       <div
-        v-show="clickToggled || hoverToggled"
+        v-show="toggled"
         class="mypa-absolute mypa-bg-white mypa-border mypa-border-solid mypa-flex mypa-flex-col mypa-right-0 mypa-top-full mypa-z-50">
         <ActionButton
-          v-for="(action, index) in dropdownActions"
+          v-for="(action, index) in dropdownActions.hidden"
           :key="`${index}_${action.id}`"
           class="!mypa-border-none"
           :action="action">
@@ -44,8 +42,8 @@
 </template>
 
 <script lang="ts">
-import {ActionButton, PdkDropdownAction, PdkIcon, useLanguage} from '@myparcel/pdk-frontend';
-import {PropType, computed, defineComponent, ref} from 'vue';
+import {ActionButton, PdkDropdownAction, PdkIcon, useDropdownData, useLanguage} from '@myparcel/pdk-frontend';
+import {PropType, computed, defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'WcDropdownButton',
@@ -70,20 +68,15 @@ export default defineComponent({
 
   emits: ['click'],
   setup: (props) => {
-    const hoverToggled = ref(false);
-    const clickToggled = ref(false);
     const {translate} = useLanguage();
+    const dropdownData = useDropdownData(props.actions);
 
     return {
-      clickToggled,
-      hoverToggled,
+      ...dropdownData,
 
       dropdownIcon: computed(() => {
-        return hoverToggled.value ? PdkIcon.ARROW_UP : PdkIcon.ARROW_DOWN;
+        return dropdownData.toggled.value ? PdkIcon.ARROW_UP : PdkIcon.ARROW_DOWN;
       }),
-
-      dropdownActions: computed(() => props.actions.filter((option) => !option.standalone)),
-      standaloneActions: computed(() => props.actions.filter((option) => option.standalone)),
 
       translate,
     };
