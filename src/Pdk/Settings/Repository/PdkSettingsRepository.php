18232f4992ca1ef4a772bb99077f056054756313
Edie Lemoine <edie@myparcel.nl>
2023-02-07 13:42:05 +0100

    fix: update settings

--- src/Pdk/Settings/Repository/PdkSettingsRepository.php
+++ src/Pdk/Settings/Repository/PdkSettingsRepository.php
@@ -4,45 +4,33 @@ declare(strict_types=1);
 
 namespace MyParcelNL\WooCommerce\Pdk\Settings\Repository;
 
-use MyParcelNL\Pdk\Base\Support\Arr;
 use MyParcelNL\Pdk\Facade\Pdk;
-use MyParcelNL\Pdk\Settings\Model\AbstractSettingsModel;
 use MyParcelNL\Pdk\Settings\Repository\AbstractSettingsRepository;
 use MyParcelNL\Sdk\src\Support\Str;
 
 class PdkSettingsRepository extends AbstractSettingsRepository
 {
     /**
-     * @param  string $name
+     * @param  string $namespace
      *
-     * @return mixed
+     * @return array
      */
-    public function get(string $name)
+    public function getGroup(string $namespace): array
     {
-        $parts = explode('.', $name);
-
-        if (count($parts) < 3) {
-            $key = $parts[0];
-        } else {
-            $key = implode('_', array_slice($parts, 0, 2));
-        }
-
-        $group = $this->retrieve(sprintf('settings_%s', $key), function () use ($key) {
-            return get_option($this->getOptionName($key), null);
+        return $this->retrieve($namespace, function () use ($namespace) {
+            return get_option($this->getOptionName($namespace), null);
         });
-
-        return Arr::get($group, end($parts));
     }
 
     /**
-     * @param  \MyParcelNL\Pdk\Settings\Model\AbstractSettingsModel $settingsModel
+     * @param  string $key
+     * @param  mixed  $value
      *
      * @return void
-     * @throws \MyParcelNL\Pdk\Base\Exception\InvalidCastException
      */
-    public function store(AbstractSettingsModel $settingsModel): void
+    protected function store(string $key, $value): void
     {
-        update_option($this->getOptionName($settingsModel->id), $settingsModel->toStorableArray());
+        update_option($this->getOptionName($key), $value);
     }
 
     /**
