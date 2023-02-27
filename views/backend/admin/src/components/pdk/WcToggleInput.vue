ba5f874969adf1b1f96ba8e95abb5e2431847e14
Edie Lemoine <edie@myparcel.nl>
2023-02-27 17:16:18 +0100

    fix(admin): update components

--- views/backend/admin/src/components/pdk/WcToggleInput.vue
+++ views/backend/admin/src/components/pdk/WcToggleInput.vue
@@ -21,17 +21,17 @@
 </template>
 
 <script lang="ts" setup>
-import {generateFieldId, useElement, useLanguage} from '@myparcel-pdk/admin/src';
+import {ElementInstance, generateFieldId, useLanguage} from '@myparcel-pdk/admin/src';
+import {InteractiveElementInstance} from '@myparcel/vue-form-builder/src';
 import {useVModel} from '@vueuse/core';
 
 // eslint-disable-next-line vue/no-unused-properties
-const props = defineProps({modelValue: {type: Boolean}});
-const emit = defineEmits(['update:modelValue']);
+const props = defineProps<{modelValue: boolean; element: InteractiveElementInstance}>();
+const emit = defineEmits<(e: 'update:modelValue', value: boolean) => void>();
 
 const model = useVModel(props, undefined, emit);
 
-const id = generateFieldId();
-const element = useElement();
+const id = generateFieldId(props.element as ElementInstance);
 
 const {translate} = useLanguage();
 </script>
