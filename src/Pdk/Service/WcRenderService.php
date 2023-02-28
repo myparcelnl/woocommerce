f1f2fa32da8648d236198a3a159a379394e77a76
Edie Lemoine <edie@myparcel.nl>
2023-02-28 15:28:02 +0100

    fix: update product settings

--- src/Pdk/Service/WcRenderService.php
+++ src/Pdk/Service/WcRenderService.php
@@ -9,6 +9,7 @@ use MyParcelNL\Pdk\Facade\LanguageService;
 use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Pdk\Facade\Settings;
 use MyParcelNL\Pdk\Frontend\Form\Components;
+use MyParcelNL\Pdk\Frontend\Settings\View\AbstractSettingsView;
 use MyParcelNL\Pdk\Frontend\Settings\View\ProductSettingsView;
 use MyParcelNL\Pdk\Plugin\Context;
 use MyParcelNL\Pdk\Plugin\Model\PdkCart;
@@ -56,7 +57,8 @@ class WcRenderService extends RenderService
     {
         try {
             $appInfo = Pdk::getAppInfo();
-            $view    = Pdk::get(ProductSettingsView::class);
+            /** @var \MyParcelNL\Pdk\Frontend\Settings\View\ProductSettingsView $productSettingsView */
+            $productSettingsView = Pdk::get(ProductSettingsView::class);
 
             ob_start();
 
@@ -64,9 +66,8 @@ class WcRenderService extends RenderService
 
             printf('<div id="%s" class="panel woocommerce_options_panel">', "{$pluginName}_product_data");
 
-            foreach ($view->toArray()['elements'] as $field) {
-                $key    = Str::snake(sprintf('%s_product_%s', $appInfo['name'], $field['name']));
-                $method = Str::snake('woocommerce_wp' . $field['$component']);
+            foreach ($productSettingsView->toArray()['elements'] as $field) {
+                $key = Str::snake(sprintf('%s_product_%s', $appInfo['name'], $field['name']));
 
                 $options = [
                     'id'    => $key,
@@ -78,11 +79,12 @@ class WcRenderService extends RenderService
                     case Components::INPUT_TRISTATE:
                         $method             = 'woocommerce_wp_select';
                         $options['options'] = [
-                            -1 => LanguageService::translate('Default'),
-                            0  => LanguageService::translate('No'),
-                            1  => LanguageService::translate('Yes'),
+                            -1 => 'option_default',
+                            0  => 'option_no',
+                            1  => 'option_yes',
                         ];
                         break;
+
                     case Components::INPUT_TOGGLE:
                         $method             = 'woocommerce_wp_checkbox';
                         $options['cbvalue'] = 1;
@@ -90,11 +92,17 @@ class WcRenderService extends RenderService
 
                     case Components::INPUT_SELECT:
                         $method             = 'woocommerce_wp_select';
-                        $options['options'] = $this->transformSelectOptions($field['options'] ?? []);
+                        $options['options'] = $this->transformSelectOptions($field['options']);
                         break;
+
                     case Components::INPUT_NUMBER:
                         $method          = 'woocommerce_wp_text_input';
                         $options['type'] = 'number';
+                        break;
+
+                    default:
+                        $method = Str::snake(sprintf('woocommerce_wp%s', $field['$component']));
+                        break;
                 }
 
                 if (isset($field['description']) && LanguageService::hasTranslation($field['description'])) {
@@ -110,7 +118,7 @@ class WcRenderService extends RenderService
         } catch (Throwable $e) {
             DefaultLogger::error('Failed to render component', [
                 'component' => self::COMPONENT_PRODUCT_SETTINGS,
-                'exception' => $e,
+                'exception' => $e->getMessage(),
             ]);
 
             return '';
@@ -124,12 +132,17 @@ class WcRenderService extends RenderService
      */
     private function transformSelectOptions(array $options): array
     {
-        $newOptions = [];
+        $flattenedArray  = array_column($options, 'label', 'value');
+        $translatedArray = LanguageService::translateArray($flattenedArray);
 
-        foreach ($options as $option) {
-            $newOptions[$option['value']] = LanguageService::translate($option['label']);
+        asort($translatedArray,SORT_NATURAL);
+
+        $noneOption = $translatedArray[AbstractSettingsView::OPTIONS_VALUE_NONE] ?? null;
+
+        if ($noneOption) {
+            $translatedArray = [AbstractSettingsView::OPTIONS_VALUE_NONE => $noneOption] + $translatedArray;
         }
 
-        return $newOptions;
+        return $translatedArray;
     }
 }
