ffe0d73d0422ae0825feefc1df43d99d1dd87291
Thijmen Wijers <thijmen.wijers@myparcel.nl>
2023-04-03 14:01:47 +0200

    refactor: WcTextInput to WcCodeEditor

---
+++ views/backend/admin/src/components/pdk/WcCodeEditor.vue
@@ -0,0 +1,16 @@
+<template>
+  <PdkTextArea
+    v-model="model"
+    style="font-family: monospace, monospace"></PdkTextArea>
+</template>
+
+<script lang="ts" setup>
+import {InteractiveElementInstance} from '@myparcel/vue-form-builder/src';
+import {useVModel} from '@vueuse/core';
+
+// eslint-disable-next-line vue/no-unused-properties
+const props = defineProps<{modelValue: string | number; element: InteractiveElementInstance}>();
+const emit = defineEmits<(e: 'update:modelValue', value: string | number) => void>();
+
+const model = useVModel(props, undefined, emit);
+</script>
