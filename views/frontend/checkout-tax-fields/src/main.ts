3f2f13e0f9780f92a832cfa1e9f2b1ba54de33d5
markernst97 <markiervc@gmail.com>
2023-04-13 15:26:38 +0200

    feat: add eori and vat fields for europlus

--- views/frontend/checkout-tax-fields/src/main.ts
+++ views/frontend/checkout-tax-fields/src/main.ts
@@ -1,8 +1,10 @@
 import {StoreListener, useCheckoutStore} from '@myparcel-woocommerce/frontend-common';
-import {showHideTaxFields} from './utils';
+import {keepTaxFieldsInSync, showHideTaxFields} from './utils';
 
 const store = useCheckoutStore();
 
 store.on(StoreListener.UPDATE, () => {
   showHideTaxFields();
 });
+
+keepTaxFieldsInSync();
