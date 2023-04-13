f38fb003332c4aae4794e7c0d1f97280a3638cd6
joerivanveen <joeri@myparcel.nl>
2023-04-13 15:12:39 +0200

    feat(checkout): sync tax fields wip

--- views/frontend/checkout-tax-fields/src/utils/keepTaxFieldsInSync.ts
+++ views/frontend/checkout-tax-fields/src/utils/keepTaxFieldsInSync.ts
@@ -1,7 +1,18 @@
+import {AddressType} from '@myparcel-woocommerce/frontend-common';
+
 export const keepTaxFieldsInSync = (): void => {
-  const billingEori = document.getElementById('billing_eori') as HTMLInputElement;
-  billingEori?.addEventListener('keyup', () => {
-    const shippingEori = document.getElementById('shipping_eori') as HTMLInputElement;
-    shippingEori.value = billingEori.value;
+  const addressTypes = [AddressType.BILLING, AddressType.SHIPPING];
+  const activate = (addressType: string, fieldtype: string) => {
+    const field = document.getElementById(`${addressType}_${fieldtype}`) as HTMLInputElement;
+    field?.addEventListener('keyup', () => {
+      const thisAddressType = addressType === AddressType.BILLING ? AddressType.SHIPPING : AddressType.BILLING; // TODO
+      const thisField = document.getElementById(`${thisAddressType}_${fieldtype}`) as HTMLInputElement;
+      thisField.value = field.value;
+    });
+  };
+
+  addressTypes.forEach((addressType) => {
+    activate(addressType, 'vat');
+    activate(addressType, 'eori');
   });
 };
