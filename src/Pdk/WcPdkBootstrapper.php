f4cb71bb689836a1bd956b4d55658f521f0716ad
Edie Lemoine <edie@myparcel.nl>
2023-04-21 10:00:14 +0200

    fix: add missing entry to container

--- src/Pdk/WcPdkBootstrapper.php
+++ src/Pdk/WcPdkBootstrapper.php
@@ -64,6 +64,7 @@ class WcPdkBootstrapper extends PdkBootstrapper
             'fieldLastName'   => value('last_name'),
             'fieldPhone'      => value('phone'),
             'fieldPostalCode' => value('postcode'),
+            'fieldRegion'     => value('region'),
             'fieldState'      => value('state'),
 
             ###
