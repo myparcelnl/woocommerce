09a18cb05f7ac3c35ce544a1ee5735fa3e899d74
Edie Lemoine <edie@myparcel.nl>
2023-07-26 10:02:41 +0200

    refactor(hooks): remove dead code

--- src/Hooks/OrderNotesHooks.php
+++ src/Hooks/OrderNotesHooks.php
@@ -5,36 +5,12 @@ declare(strict_types=1);
 namespace MyParcelNL\WooCommerce\Hooks;
 
 use MyParcelNL\Pdk\App\Api\Backend\PdkBackendActions;
-use MyParcelNL\Pdk\App\Order\Contract\PdkOrderRepositoryInterface;
 use MyParcelNL\Pdk\Facade\Actions;
-use MyParcelNL\Pdk\Fulfilment\Repository\OrderNotesRepository;
 use MyParcelNL\WooCommerce\Hooks\Contract\WordPressHooksInterface;
 use WC_Order;
 
 final class OrderNotesHooks implements WordPressHooksInterface
 {
-    /**
-     * @var \MyParcelNL\Pdk\Fulfilment\Repository\OrderNotesRepository
-     */
-    private $orderNotesRepository;
-
-    /**
-     * @var \MyParcelNL\Pdk\App\Order\Contract\PdkOrderRepositoryInterface
-     */
-    private $orderRepository;
-
-    /**
-     * @param  \MyParcelNL\Pdk\App\Order\Contract\PdkOrderRepositoryInterface $pdkOrderRepository
-     * @param  \MyParcelNL\Pdk\Fulfilment\Repository\OrderNotesRepository     $orderNotesRepository
-     */
-    public function __construct(
-        PdkOrderRepositoryInterface $pdkOrderRepository,
-        OrderNotesRepository        $orderNotesRepository
-    ) {
-        $this->orderRepository      = $pdkOrderRepository;
-        $this->orderNotesRepository = $orderNotesRepository;
-    }
-
     /**
      * @param  int       $commentId
      * @param  \WC_Order $order
