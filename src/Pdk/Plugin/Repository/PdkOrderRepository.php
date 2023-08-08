01ac4eefce57d979177c23fb23cf8e3c24b58f63
Mark Ernst <33826576+Mark-Ernst@users.noreply.github.com>
2023-08-08 13:37:02 +0200

    fix(orders): do not add return barcodes to order notes (#998)

--- src/Pdk/Plugin/Repository/PdkOrderRepository.php
+++ src/Pdk/Plugin/Repository/PdkOrderRepository.php
@@ -126,17 +126,21 @@ class PdkOrderRepository extends AbstractPdkOrderRepository
      */
     private function addBarcodesToOrderNote(WC_Order $wcOrder, PdkOrder $order): void
     {
-        $withBarcodes = $order->shipments->where('barcode', '!=', null);
+        $shipments = $order->shipments
+            ->where('barcode', '!=', null)
+            ->where('isReturn', false);
 
-        if ($withBarcodes->isNotEmpty()) {
-            $prefix = Settings::get(GeneralSettings::BARCODE_IN_NOTE_TITLE, GeneralSettings::ID);
-
-            $barcodeArray = $withBarcodes
-                ->pluck('barcode')
-                ->toArray();
-
-            $wcOrder->add_order_note($prefix . implode(', ', $barcodeArray));
+        if ($shipments->isEmpty()) {
+            return;
         }
+
+        $prefix = Settings::get(GeneralSettings::BARCODE_IN_NOTE_TITLE, GeneralSettings::ID);
+
+        $barcodeArray = $shipments
+            ->pluck('barcode')
+            ->toArray();
+
+        $wcOrder->add_order_note($prefix . implode(', ', $barcodeArray));
     }
 
     /**
