6d9d7fda0f621f3189adbc2f3a636a414f9764ee
Edie Lemoine <edie@myparcel.nl>
2023-08-09 11:17:14 +0200

    fix(view): check if is child product page

--- src/Pdk/Service/WcViewService.php
+++ src/Pdk/Service/WcViewService.php
@@ -22,6 +22,14 @@ class WcViewService extends AbstractViewService
     /**
      * @return bool
      */
+    public function isChildProductPage(): bool
+    {
+        return isset($_POST['action']) && 'woocommerce_load_variations' === $_POST['action'];
+    }
+
+    /**
+     * @return bool
+     */
     public function isOrderListPage(): bool
     {
         return Pdk::get('orderListPageId') === $this->getScreenId();
@@ -48,7 +56,7 @@ class WcViewService extends AbstractViewService
      */
     public function isProductPage(): bool
     {
-        return in_array($this->getScreenId(), ['product', '']);
+        return 'product' === $this->getScreenId();
     }
 
     /**
