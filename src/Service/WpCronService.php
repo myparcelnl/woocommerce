8afc04a4a44d268aeb5dcfc39dd2d720a4fbbe2b
Edie Lemoine <edie@myparcel.nl>
2023-08-09 10:31:03 +0200

    test: fix error in tests

--- src/Service/WpCronService.php
+++ src/Service/WpCronService.php
@@ -9,24 +9,24 @@ use MyParcelNL\Pdk\Base\Contract\CronServiceInterface;
 class WpCronService implements CronServiceInterface
 {
     /**
-     * @param  callable $callback
-     * @param           ...$args
+     * @param  callable|string|callable-string $callback
+     * @param                                  ...$args
      *
      * @return void
      */
-    public function dispatch(callable $callback, ...$args): void
+    public function dispatch($callback, ...$args): void
     {
         $this->schedule($callback, time(), ...$args);
     }
 
     /**
-     * @param  callable $callback
-     * @param  int      $timestamp
-     * @param           ...$args
+     * @param  callable|string|callable-string $callback
+     * @param  int                             $timestamp
+     * @param                                  ...$args
      *
      * @return void
      */
-    public function schedule(callable $callback, int $timestamp, ...$args): void
+    public function schedule($callback, int $timestamp, ...$args): void
     {
         wp_schedule_single_event($timestamp, $callback, $args);
     }
