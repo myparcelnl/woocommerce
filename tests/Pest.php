a44d2163c6f8998fd33ffd4695c602d30f87f53a
Edie Lemoine <edie@myparcel.nl>
2023-09-07 14:50:08 +0200

    test: clean up mocked classes and functions

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,6 +15,9 @@ use function MyParcelNL\Pdk\Tests\usesShared;
 
 require __DIR__ . '/../vendor/myparcelnl/pdk/tests/Pest.php';
 require __DIR__ . '/mock_class_map.php';
+require __DIR__ . '/mock_wp_functions.php';
 require __DIR__ . '/functions.php';
+
+const WP_DEBUG = true;
 
 usesShared(new ClearContainerCache())->in(__DIR__);
