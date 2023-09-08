73a5c82e4dd459294d3aabb6b4f9ea7920efefcc
joerivanveen <joeri@myparcel.nl>
2023-09-08 13:56:35 +0200

    fix(migration): prevent api key from disappearing

--- src/Migration/Pdk/SettingsMigration.php
+++ src/Migration/Pdk/SettingsMigration.php
@@ -95,7 +95,6 @@ final class SettingsMigration extends AbstractPdkMigration
         }
 
         $settings = new Settings($newSettings);
-
         $settingsRepository->storeAllSettings($settings);
     }
 
@@ -354,7 +353,7 @@ final class SettingsMigration extends AbstractPdkMigration
                         $method = sprintf('%s:%s', $parts[0], $parts[1]);
                     }
 
-                    if ($parts[1] > 10 && Str::startsWith($item, self::PREFIX_FLAT_RATE)) {
+                    if (isset($parts[1]) && $parts[1] > 10 && Str::startsWith($item, self::PREFIX_FLAT_RATE)) {
                         $carry[] = $method;
                         $method  = sprintf('%s%s', self::PREFIX_FLAT_RATE, $parts[1][0]);
                     }
--- src/Service/WpInstallerService.php
+++ src/Service/WpInstallerService.php
@@ -19,6 +19,21 @@ final class WpInstallerService extends InstallerService
     }
 
     /**
+     * Override because a null version will re-trigger a migration or overwrite all options with defaults
+     * when deactivating and re-activating the plugin in WordPress.
+     *
+     * @param  null|string $version
+     *
+     * @return void
+     */
+    protected function updateInstalledVersion(?string $version): void
+    {
+        if ($version) {
+            parent::updateInstalledVersion($version);
+        }
+    }
+
+    /**
      * This is not in the PDK config or the bootstrapper because it's legacy stuff.
      *
      * @return null|string
