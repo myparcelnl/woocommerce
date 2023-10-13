4fade9ae741b88c49c2c25f14feb57ae530e1f33
Mark Ernst <33826576+Mark-Ernst@users.noreply.github.com>
2023-10-13 16:38:56 +0200

    fix(compatibility): reduce conflicts with other plugins (#1025)

---
+++ pdk.config.js
@@ -0,0 +1,85 @@
+import fs from 'fs';
+import glob from 'fast-glob';
+import {getPlatformDistPath, executePromises} from '@myparcel-pdk/app-builder';
+import path from 'path';
+
+/**
+ * @type {import('@myparcel-pdk/app-builder').PdkBuilderConfig}
+ */
+export default {
+  name: 'woocommerce',
+  platforms: ['myparcelnl', 'myparcelbe'],
+  source: [
+    '!**/node_modules/**',
+    '.cache/build/composer.json',
+    '.cache/build/config/**/*',
+    '.cache/build/src/**/*',
+    '.cache/build/vendor/**/*',
+    '.cache/build/woocommerce-myparcel.php',
+    'views/**/lib/**/*',
+    'CONTRIBUTING.md',
+    'LICENSE.txt',
+    'README.md',
+    'readme.txt',
+    'wpm-config.json',
+  ],
+
+  platformFolderName(platform) {
+    switch (platform) {
+      case 'myparcelnl':
+        return 'woocommerce-myparcel';
+
+      case 'myparcelbe':
+        return 'wc-myparcel-belgium';
+    }
+
+    return '{{name}}';
+  },
+
+  versionSource: [
+    {path: 'package.json'},
+    {path: 'composer.json'},
+    {path: 'woocommerce-myparcel.php', regex: /Version:\s*(.+)/},
+    // TODO: Uncomment when this version is stable.
+    // {path: 'readme.txt', regex: /Stable tag:\s*(.+)/},
+  ],
+
+  composerCommand: 'docker compose run --rm -T php composer',
+
+  translations: {
+    // eslint-disable-next-line no-magic-numbers
+    additionalSheet: 535277615,
+  },
+
+  hooks: {
+    async afterCopy(args) {
+      const {config, env, debug} = args.context;
+
+      debug('Copying scoped build files to root');
+
+      await executePromises(
+        args,
+        config.platforms.map(async(platform) => {
+          const platformDistPath = getPlatformDistPath({config, env, platform});
+
+          const files = glob.sync('.cache/build/**/*', {cwd: platformDistPath});
+
+          await Promise.all(
+            files.map(async(file) => {
+              const oldPath = `${platformDistPath}/${file}`;
+              const newPath = oldPath.replace('.cache/build/', '');
+
+              if (!args.dryRun) {
+                await fs.promises.mkdir(path.dirname(newPath), {recursive: true});
+                await fs.promises.rename(oldPath, newPath);
+              }
+            }),
+          );
+
+          await fs.promises.rm(`${platformDistPath}/.cache`, {recursive: true});
+        }));
+
+      debug('Copied scoped build files to root.');
+    },
+  },
+};
---
+++ scoper.inc.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Isolated\Symfony\Component\Finder\Finder;
+
+// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md
+return [
+    'prefix'  => 'MyParcelNL',
+    'finders' => [
+        Finder::create()
+            ->append(
+                [
+                    'woocommerce-myparcel.php',
+                    'composer.json',
+                ]
+            ),
+        Finder::create()
+            ->files()
+            ->in(['src', 'config']),
+        Finder::create()
+            ->files()
+            ->ignoreVCS(true)
+            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.lock/')
+            ->exclude(
+                [
+                    'test',
+                    'tests',
+                    'Tests',
+                    'vendor-bin',
+                ]
+            )
+            ->in('vendor'),
+    ],
+
+    'exclude-namespaces' => [
+        // Exclude global namespace
+        '/^$/',
+        'WP',
+        'Automattic',
+        'MyParcelNL',
+    ],
+];
