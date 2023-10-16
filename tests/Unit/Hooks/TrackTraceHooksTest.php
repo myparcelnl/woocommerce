842a44e8609d312576b28133894a77c13f4ae37d
Edie Lemoine <edie@myparcel.nl>
2023-10-16 11:12:36 +0200

    test: refactor track trace test to use factories

--- tests/Unit/Hooks/TrackTraceHooksTest.php
+++ tests/Unit/Hooks/TrackTraceHooksTest.php
@@ -6,27 +6,21 @@ declare(strict_types=1);
 namespace MyParcelNL\WooCommerce\Hooks;
 
 use MyParcelNL\Pdk\Facade\Pdk;
-use MyParcelNL\Pdk\Settings\Contract\SettingsRepositoryInterface;
 use MyParcelNL\Pdk\Settings\Model\OrderSettings;
-use MyParcelNL\Pdk\Tests\Bootstrap\MockSettingsRepository;
 use MyParcelNL\WooCommerce\Tests\Uses\UsesMockWcPdkInstance;
 use WC_Order_Factory;
-use function DI\autowire;
+use function MyParcelNL\Pdk\Tests\factory;
 use function MyParcelNL\Pdk\Tests\usesShared;
 use function Spatie\Snapshots\assertMatchesHtmlSnapshot;
 
-usesShared(
-    new UsesMockWcPdkInstance([
-        SettingsRepositoryInterface::class => autowire(MockSettingsRepository::class)->constructor([
-            OrderSettings::ID => [
-                OrderSettings::TRACK_TRACE_IN_ACCOUNT => true,
-                OrderSettings::TRACK_TRACE_IN_EMAIL   => true,
-            ],
-        ]),
-    ])
-);
+usesShared(new UsesMockWcPdkInstance());
 
 it('renders order details in account', function (WC_Order_Factory $factory) {
+    factory(OrderSettings::class)
+        ->withTrackTraceInAccount(true)
+        ->withTrackTraceInEmail(true)
+        ->store();
+
     /** @var TrackTraceHooks $class */
     $class = Pdk::get(TrackTraceHooks::class);
 
