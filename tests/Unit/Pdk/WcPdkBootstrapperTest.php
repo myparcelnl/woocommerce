a0537c20ad8f639907b70d5664faae09cba1099f
joerivanveen <joeri@myparcel.nl>
2023-10-31 14:20:25 +0100

    fix(test): implement feedback

---
+++ tests/Unit/Pdk/WcPdkBootstrapperTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\WooCommerce\Pdk;
+
+use MyParcelNL\Pdk\Facade\Pdk;
+use MyParcelNL\WooCommerce\Tests\Mock\MockWpUser;
+use MyParcelNL\WooCommerce\Tests\Uses\UsesMockWcPdkInstance;
+use function MyParcelNL\Pdk\Tests\usesShared;
+
+usesShared(new UsesMockWcPdkInstance());
+
+it('returns proper permission callback', function (array $roles, bool $expected) {
+    foreach ($roles as $role) {
+        MockWpUser::addRole($role);
+    }
+
+    $actual = (Pdk::get('routeBackendPermissionCallback'))();
+
+    expect($actual)->toBe($expected);
+})->with([
+    'no roles'                    => [
+        'roles'    => [],
+        'expected' => false,
+    ],
+    'no administrator'            => [
+        'roles'    => ['subscriber'],
+        'expected' => false,
+    ],
+    'nobody, subscriber'          => [
+        'roles'    => ['nobody', 'subscriber'],
+        'expected' => false,
+    ],
+    'administrator'               => [
+        'roles'    => ['administrator'],
+        'expected' => true,
+    ],
+    'shop manager'                => [
+        'roles'    => ['shop_manager'],
+        'expected' => true,
+    ],
+    'subscriber, shop manager'    => [
+        'roles'    => ['subscriber', 'shop_manager'],
+        'expected' => true,
+    ],
+    'subscriber, administrator'   => [
+        'roles'    => ['subscriber', 'administrator'],
+        'expected' => true,
+    ],
+    'shop manager, administrator' => [
+        'roles'    => ['shop_manager', 'administrator'],
+        'expected' => true,
+    ],
+    'shop manager, subscriber'    => [
+        'roles'    => ['shop_manager', 'subscriber'],
+        'expected' => true,
+    ],
+    'many, shop_manager'          => [
+        'roles'    => ['nobody', 'subscriber', 'anybody', 'shop_manager'],
+        'expected' => true,
+    ],
+    'many, shop_manager, any'     => [
+        'roles'    => ['nobody', 'subscriber', 'anybody', 'shop_manager', 'somebody'],
+        'expected' => true,
+    ],
+]);
