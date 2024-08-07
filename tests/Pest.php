84b95a2cc5e680684782906183bb8b683de1f5f5
Edie Lemoine <edie@myparcel.nl>
2024-08-07 11:36:23 +0200

    test: fix container cache clearing

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,7 +15,6 @@ use MyParcelNL\WooCommerce\Tests\Mock\MockWcData;
 use MyParcelNL\WooCommerce\Tests\Mock\MockWcPdkBootstrapper;
 use MyParcelNL\WooCommerce\Tests\Mock\MockWpActions;
 use MyParcelNL\WooCommerce\Tests\Mock\MockWpMeta;
-use function MyParcelNL\Pdk\Tests\usesShared;
 
 require __DIR__ . '/../vendor/myparcelnl/pdk/tests/Pest.php';
 require __DIR__ . '/mock_class_map.php';
@@ -24,8 +23,6 @@ require __DIR__ . '/mock_wp_functions.php';
 require __DIR__ . '/mock_wc_functions.php';
 
 define('WP_DEBUG', true);
-
-usesShared(new ClearContainerCache())->in(__DIR__);
 
 uses()
     ->afterEach(function () {
