ddada76e7c15d52dcd469814ea876e96af16f3c5
Edie Lemoine <edie@myparcel.nl>
2024-10-02 17:02:36 +0200

    feat(admin): add download logs action
    Needs myparcelnl/pdk#288
    Needs myparcelnl/js-pdk#221

--- src/Pdk/Hooks/AbstractPdkEndpointHooks.php
+++ src/Pdk/Hooks/AbstractPdkEndpointHooks.php
@@ -8,7 +8,6 @@ use MyParcelNL\Pdk\App\Api\PdkEndpoint;
 use MyParcelNL\WooCommerce\Hooks\Concern\UsesPdkRequestConverter;
 use MyParcelNL\WooCommerce\Hooks\Contract\WordPressHooksInterface;
 use WP_REST_Request;
-use WP_REST_Response;
 
 abstract class AbstractPdkEndpointHooks implements WordPressHooksInterface
 {
@@ -28,23 +27,23 @@ abstract class AbstractPdkEndpointHooks implements WordPressHooksInterface
     }
 
     /**
-     * Convert the request to a format that the PDK understands, calls the endpoint and converts the response back to
-     * a format that WordPress understands.
+     * Convert the request to a format that the PDK understands, calls the endpoint and sends the response.
      * Disables notices to prevent random plugin notices from breaking the response.
      *
      * @param  string           $context
      * @param  \WP_REST_Request $request
      *
-     * @return \WP_REST_Response
+     * @return void
      */
-    protected function processRequest(string $context, WP_REST_Request $request): WP_REST_Response
+    protected function processRequest(string $context, WP_REST_Request $request): void
     {
         error_reporting(error_reporting() & ~E_NOTICE);
 
         $convertedRequest = $this->convertRequest($request);
+        $response         = $this->endpoint->call($convertedRequest, $context);
 
-        $response = $this->endpoint->call($convertedRequest, $context);
+        $response->send();
 
-        return $this->convertResponse($response);
+        exit;
     }
 }
