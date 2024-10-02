7031a905f1a4241430c7801e32bba7c57df802fd
Edie Lemoine <edie@myparcel.nl>
2024-10-02 17:02:36 +0200

    feat(admin): add download logs action
    Needs myparcelnl/pdk#288
    Needs myparcelnl/js-pdk#221

--- src/Logger/WcLogger.php
+++ src/Logger/WcLogger.php
@@ -4,11 +4,34 @@ declare(strict_types=1);
 
 namespace MyParcelNL\WooCommerce\Logger;
 
+use Automattic\WooCommerce\Utilities\LoggingUtil;
 use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Pdk\Logger\AbstractLogger;
+use WC_Logger_Interface;
 
 class WcLogger extends AbstractLogger
 {
+    /**
+     * @return array
+     */
+    public function getLogFiles(): array
+    {
+        $logger = $this->getWcLogger();
+
+        if (! $logger) {
+            return parent::getLogFiles();
+        }
+
+        $logDirectory = trailingslashit(LoggingUtil::get_log_directory());
+        $foundFiles   = glob(sprintf('%s%s-*.log', $logDirectory, $this->getLogPrefix()));
+
+        if (! $foundFiles) {
+            return parent::getLogFiles();
+        }
+
+        return $foundFiles;
+    }
+
     /**
      * @param        $level
      * @param        $message
@@ -18,7 +41,7 @@ class WcLogger extends AbstractLogger
      */
     public function log($level, $message, array $context = []): void
     {
-        $logger = function_exists('wc_get_logger') ? wc_get_logger() : null;
+        $logger = $this->getWcLogger();
 
         if (! $logger) {
             return;
@@ -26,6 +49,22 @@ class WcLogger extends AbstractLogger
 
         $json = count($context) ? PHP_EOL . json_encode($context, JSON_PRETTY_PRINT) : '';
 
-        $logger->log($level, $message . $json, ['source' => Pdk::getAppInfo()->name]);
+        $logger->log($level, $message . $json, ['source' => $this->getLogPrefix()]);
+    }
+
+    /**
+     * @return string
+     */
+    private function getLogPrefix(): string
+    {
+        return Pdk::getAppInfo()->name;
+    }
+
+    /**
+     * @return null|\WC_Logger_Interface
+     */
+    private function getWcLogger(): ?WC_Logger_Interface
+    {
+        return function_exists('wc_get_logger') ? wc_get_logger() : null;
     }
 }
