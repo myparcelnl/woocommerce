6554528dd8a367bc7b3199f09e935598a9205d32
Edie Lemoine <edie@myparcel.nl>
2024-10-23 14:57:04 +0200

    build: improve blocks further

--- views/blocks/delivery-options/src/index.tsx
+++ views/blocks/delivery-options/src/index.tsx
@@ -1,10 +1,12 @@
 import {box} from '@wordpress/icons';
 import {registerBlockType} from '@wordpress/blocks';
 import metadata from '../block.json';
+import packageJson from '../../../../package.json';
 import {Save} from './save';
 import {Edit} from './edit';
 
 registerBlockType(metadata, {
+  version: packageJson.version,
   icon: {
     src: box,
   },
--- views/blocks/delivery-options/webpack.config.js
+++ views/blocks/delivery-options/webpack.config.js
@@ -1,29 +1,3 @@
-const path = require('path');
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
+import {createWebpackConfig} from '@myparcel-woocommerce/webpack-config';
 
-/**
- * @type {import('webpack').Configuration}
- */
-const config = {
-  ...defaultConfig,
-
-  devtool: 'inline-source-map',
-
-  output: {
-    ...defaultConfig.output,
-    path: path.resolve(process.cwd(), 'dist'),
-  },
-
-  entry: {
-    'myparcelnl-delivery-options-block': path.resolve(process.cwd(), 'src', 'index.tsx'),
-    'myparcelnl-delivery-options-block-frontend': path.resolve(process.cwd(), 'src', 'frontend.tsx'),
-  },
-
-  plugins: [
-    ...defaultConfig.plugins.filter((plugin) => plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'),
-    new WooCommerceDependencyExtractionWebpackPlugin(),
-  ],
-};
-
-module.exports = config;
+export default createWebpackConfig();
