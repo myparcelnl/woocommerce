4382367e568f3b688b53e3885f8aa4280774cf32
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-04-23 13:27:00 +0200

    feat(address): only load the address widget if enabled in settings
    fixes INT-935

--- src/Hooks/CheckoutScriptHooks.php
+++ src/Hooks/CheckoutScriptHooks.php
@@ -50,7 +50,7 @@ final class CheckoutScriptHooks implements WordPressHooksInterface
             return;
         }
         $this->loadCoreScripts();
-        // $this->loadSeparateAddressFieldsScripts();
+        $this->loadSeparateAddressFieldsScripts();
         $this->loadAddressWidgetScript();
         $this->loadDeliveryOptionsScripts();
         $this->loadTaxFieldsScripts();
@@ -58,17 +58,20 @@ final class CheckoutScriptHooks implements WordPressHooksInterface
 
     public function loadAddressWidgetScript(): void
     {
-        // @TODO figure out how DO is loading Vue3 in the checkout and emulate that
+        if (! Settings::get(CheckoutSettings::ENABLE_ADDRESS_WIDGET, CheckoutSettings::ID)) {
+            return;
+        }
+
         $this->service->enqueueVue('3');
 
         $this->service->enqueueLocalScript(
-            'myparcelnl-checkout-address-widget', // TODO: set from WPScriptService
+            'myparcelnl-checkout-address-widget',
             'views/frontend/checkout-address-widget/dist/address-widget',
             $this->getWcCheckoutDependencies()
         );
 
         $this->service->enqueueLocalStyle(
-            'myparcelnl-checkout-address-widget', // TODO: set from WPScriptService
+            'myparcelnl-checkout-address-widget',
             'views/frontend/checkout-address-widget/dist/style.css'
         );
 
