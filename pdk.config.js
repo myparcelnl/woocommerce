8a9c9c0d59687f37c6266e93af1e0b2e003ca10d
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-07-18 16:46:00 +0200

    build: remove platform-specific build rules

--- pdk.config.js
+++ pdk.config.js
@@ -1,21 +1,12 @@
-import {spawnSync} from 'node:child_process';
+import { spawnSync } from 'node:child_process';
 import {
-  PdkPlatformName,
-  addPlatformToContext,
   defineConfig,
-  exists,
-  getPlatformDistPath,
-  renameFile,
-  reportFileDoesNotExist,
-  resolvePath,
-  resolveString,
 } from '@myparcel-pdk/app-builder';
 
 const ENTRY_FILE = 'woocommerce-myparcel.php';
 
 export default defineConfig({
-  name: 'woocommerce',
-  platforms: [PdkPlatformName.MyParcelNl, PdkPlatformName.MyParcelBe],
+  name: 'woocommerce-myparcel',
   source: [
     '!**/node_modules/**',
     'views/**/dist/**/*',
@@ -27,27 +18,13 @@ export default defineConfig({
     'wpm-config.json',
   ],
 
-  platformFolderName(platform) {
-    switch (platform) {
-      case PdkPlatformName.MyParcelNl:
-        return 'woocommerce-myparcel';
-
-      case PdkPlatformName.MyParcelBe:
-        return 'wc-myparcel-belgium';
-    }
-
-    return '{{name}}';
-  },
-
   versionSource: [
-    {path: 'package.json'},
-    {path: 'composer.json'},
-    {path: ENTRY_FILE, regex: /Version:\s*(.+)/},
+    { path: 'package.json' },
+    { path: 'composer.json' },
+    { path: ENTRY_FILE, regex: /Version:\s*(.+)/ },
     // TODO: Uncomment when this version is stable.
     // {path: 'readme.txt', regex: /Stable tag:\s*(.+)/},
   ],
-
-  rootCommand: 'docker compose run --rm -T php',
 
   translations: {
     // eslint-disable-next-line no-magic-numbers
@@ -66,30 +43,6 @@ export default defineConfig({
       if (buffer.status !== 0) {
         throw new Error('Build failed.');
       }
-    },
-
-    /**
-     * Rename the entry file.
-     */
-    async afterCopy({context}) {
-      const {config} = context;
-
-      await Promise.all(
-        config.platforms.map(async(platform) => {
-          const platformContext = addPlatformToContext(context, platform);
-          const platformDistPath = getPlatformDistPath(platformContext);
-          const sourcePath = resolvePath([platformDistPath, ENTRY_FILE], context);
-
-          if (!await exists(sourcePath)) {
-            reportFileDoesNotExist(sourcePath, platformContext);
-            return;
-          }
-
-          const newFilename = `${resolveString(config.platformFolderName, platformContext)}.php`;
-
-          await renameFile(sourcePath, sourcePath.replace(ENTRY_FILE, newFilename), platformContext);
-        }),
-      );
     },
   },
 });
