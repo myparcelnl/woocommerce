19788a21bd8cd38d071639bfc26e5a2a442ae34e
Joeri van Veen <joeri@myparcel.nl>
2025-12-04 10:52:59 +0100

    fix: use correct address for order after checkout

--- src/Adapter/WcAddressAdapter.php
+++ src/Adapter/WcAddressAdapter.php
@@ -47,6 +47,7 @@ class WcAddressAdapter
 
         return array_merge(
             $this->getAddressFields($order, $resolvedAddressType),
+            $this->getSeparateAddressFromOrder($order, $resolvedAddressType),
             [
                 'eoriNumber' => $this->getOrderMeta($order, Pdk::get('fieldEoriNumber'), $resolvedAddressType),
                 'vatNumber'  => $this->getOrderMeta($order, Pdk::get('fieldVatNumber'), $resolvedAddressType),
@@ -143,6 +144,34 @@ class WcAddressAdapter
                 $this->getAddressField($instance, Pdk::get('fieldLastName'), $addressType),
             ])
         );
+    }
+
+    /**
+     * @param  \WC_Order $order
+     * @param  string    $addressType
+     *
+     * @return array
+     */
+    private function getSeparateAddressFromOrder(WC_Order $order, string $addressType): array
+    {
+        // Check if the hidden address is filled, use it when available and don't send the fullStreet in that case.
+        if ($this->getOrderMeta($order, Pdk::get('checkoutAddressHiddenInputName'), $addressType)) {
+            return [];
+        }
+        // if there is already an addres, which might be updated in the admin, use that
+        if ($this->getAddressField($order, Pdk::get('fieldAddress1'), $addressType)) {
+            return [];
+        }
+        $street       = $this->getOrderMeta($order, Pdk::get('fieldStreet'), $addressType);
+        $number       = $this->getOrderMeta($order, Pdk::get('fieldNumber'), $addressType);
+        $numberSuffix = $this->getOrderMeta($order, Pdk::get('fieldNumberSuffix'), $addressType);
+        $country = $this->getAddressField($order, Pdk::get('fieldCountry'), $addressType);
+
+        $hasSeparateAddress = $street || $number || $numberSuffix;
+
+        return $hasSeparateAddress && in_array($country, Pdk::get('countriesWithSeparateAddressFields'), true)
+            ? ['fullStreet' => trim("$street $number $numberSuffix")]
+            : [];
     }
 
     /**
