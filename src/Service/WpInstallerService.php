1cf34de21e7def733c708f60ae01b8dacd3b01da
Nabil Azahaf <nabil.azahaf@myparcel.nl>
2026-07-30 09:02:06 +0200

    fix: narrow settings restore

--- src/Service/WpInstallerService.php
+++ src/Service/WpInstallerService.php
@@ -58,20 +58,7 @@ final class WpInstallerService extends InstallerService
      */
     protected function getInstalledVersion(): ?string
     {
-        $versions = [
-            get_option('_myparcelcom_installed_version', null), // v6+
-            get_option('_myparcelnl_installed_version', null),  // v5 - nl
-            get_option('_myparcelbe_installed_version', null),  // v5 - be
-            get_option('woocommerce_myparcel_version', null),   // v4
-        ];
-
-        foreach ($versions as $version) {
-            if (is_string($version) && '' !== $version) {
-                return $version;
-            }
-        }
-
-        return null;
+        return parent::getInstalledVersion() ?: $this->getLegacyInstalledVersion();
     }
 
     /**
@@ -89,5 +76,22 @@ final class WpInstallerService extends InstallerService
         }
 
         parent::updateInstalledVersion($version);
+    }
+
+    /**
+     * This is not in the PDK config or the bootstrapper because it's legacy stuff.
+     *
+     * @return null|string
+     */
+    private function getLegacyInstalledVersion(): ?string
+    {
+        // Get the legacy installed version, prioritized by:
+        // 1. v5 - nl
+        // 2. v5 - be
+        // 3. v4
+        // Whichever matches first is used
+        return get_option('_myparcelnl_installed_version', null) // v5 - nl
+            ?? get_option('_myparcelbe_installed_version', null) // v5 - be
+            ?? get_option('woocommerce_myparcel_version', null); // v4
     }
 }
